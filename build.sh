5a98ef653293d5d3a9b4f0920d3625291a93601a
ER <eino.rauhala@proton.me>
2023-04-02 12:38:06 +0300

    fix: remove -y flag from yq; not in  go version

--- build.sh
+++ build.sh
@@ -13,9 +13,9 @@ echo "---"
 pip install --prefix=/usr yafti
 
 # add a package group for yafti using the packages defined in recipe.yml
-yq -iy '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /etc/yafti.yml
-yq -iy '.screens.applications.values.groups.Custom.default = true' /etc/yafti.yml
+yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /etc/yafti.yml
+yq -i '.screens.applications.values.groups.Custom.default = true' /etc/yafti.yml
 flatpaks=$(yq '.flatpaks[]' < /etc/ublue-recipe.yml)
 for pkg in $flatpaks; do \
-    yq -iy ".screens.applications.values.groups.Custom.packages += [{'$pkg': '$pkg'}]" /etc/yafti.yml \
+    yq -i ".screens.applications.values.groups.Custom.packages += [{'$pkg': '$pkg'}]" /etc/yafti.yml \
 done

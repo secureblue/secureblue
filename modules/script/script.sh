e03ac5de80b62f25db8031bf1a31a395e18ff3cf
ER <eino.rauhala@proton.me>
2023-08-13 16:08:31 +0300

    feat: script runner module

--- modules/script/script.sh
+++ modules/script/script.sh
@@ -3,4 +3,11 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-echo "$1"
+readarray RUN < <(yq -o=j -I=0 '.run[]' "$1" )
+
+cd "$CONFIG_DIRECTORY/scripts"
+
+for CMD in "${RUN[@]}"; do
+    echo "Running command: $CMD"
+    $CMD
+done

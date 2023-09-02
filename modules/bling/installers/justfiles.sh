d2ab95c9e44116d07afa6b437bbcf4437ac11a05
ER <eino.rauhala@proton.me>
2023-09-02 20:52:34 +0300

    fix: bling justfile installer nested just dir idon't create another just directory inside the basic just directory, just copy the contents

--- modules/bling/installers/justfiles.sh
+++ modules/bling/installers/justfiles.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-cp -r "$BLING_DIRECTORY/files/usr/share/ublue-os/just" "/usr/share/ublue-os/just"
+cp -r "$BLING_DIRECTORY/files/usr/share/ublue-os/just/*" "/usr/share/ublue-os/just"

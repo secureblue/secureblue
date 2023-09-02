e5fdb304b4e2c0a2933288b5b4d336ab5a39d061
ER <eino.rauhala@proton.me>
2023-09-02 20:55:49 +0300

    fix: some more bling installer file copy issues (brainfarts), the nix install one is just...

--- modules/bling/installers/nix-installer.sh
+++ modules/bling/installers/nix-installer.sh
@@ -3,4 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-cp -r "$BLING_DIRECTORY/files/usr/bin/ublue-nix-install" "/usr/bin/ublue-nix-uninstall"
+cp "$BLING_DIRECTORY/files/usr/bin/ublue-nix-install" "/usr/bin/ublue-nix-install"
+cp "$BLING_DIRECTORY/files/usr/bin/ublue-nix-uninstall" "/usr/bin/ublue-nix-uninstall"

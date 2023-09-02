2208ea88e50bb504c440783912dd0cd56f62d008
ER <eino.rauhala@proton.me>
2023-09-02 12:27:38 +0300

    fix: remove ub update services in ub updater inst

--- modules/bling/installers/ublue-update.sh
+++ modules/bling/installers/ublue-update.sh
@@ -3,4 +3,8 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+# Check if ublue-os-update-services rpm is installed, these services conflict with ublue-update
+if rpm -q ublue-os-update-services > /dev/null; then
+    rpm-ostree override remove ublue-os-update-services
+fi
 rpm-ostree install "$BLING_DIRECTORY"/rpms/ublue-update*.rpm

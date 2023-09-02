eb37ec35b4723fe1ea0d748e0a768ea39e0017a8
ER <eino.rauhala@proton.me>
2023-09-02 20:59:04 +0300

    fix: move globs outside of ""

--- modules/bling/installers/fonts.sh
+++ modules/bling/installers/fonts.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-cp -r "$BLING_DIRECTORY/files/usr/share/fonts/*" "/usr/share/fonts"
+cp -r "$BLING_DIRECTORY"/files/usr/share/fonts/* "/usr/share/fonts"

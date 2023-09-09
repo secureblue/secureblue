c3bc555ff4673a61588778e2d453332873f7191d
ER <eino.rauhala@proton.me>
2023-09-09 12:41:14 +0300

    feat: enable dconf update service in installer

--- modules/bling/installers/dconf-update-service.sh
+++ modules/bling/installers/dconf-update-service.sh
@@ -4,3 +4,4 @@
 set -oue pipefail
 
 cp -r "$BLING_DIRECTORY/files/usr/etc/systemd/system/dconf-update.service" "/usr/etc/systemd/system/dconf-update.service"
+systemctl enable dconf-update.services

ff4ccd8e950f927c6fdcd0998a9d68a60f56e5c8
qoijjj <pmquinn5@proton.me>
2023-09-11 00:59:28 -0700

    override replace

---
+++ scripts/mesa.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+rpm-ostree override remove mesa-va-drivers-freeworld --install mesa-va-drivers

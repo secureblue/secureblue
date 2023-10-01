f94db2f5fd29f368d6484fbe19c54cf7d35a6bac
Payton Quinn <pmquinn5@proton.me>
2023-10-01 00:59:47 -0700

    try again

--- modules/repo-files.sh
+++ modules/repo-files.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-RECIPE_FILE="/usr/share/ublue-os/recipe.yml"
-get_yaml_array() {
-    mapfile -t "${1}" < <(yq -- "${2}" "${RECIPE_FILE}")
-}
-
 # Add repo files.
-get_yaml_array repos '.rpm.repo-files[]'
+get_yaml_array repos '.repos[]'
 if [[ ${#repos[@]} -gt 0 ]]; then
     echo "-- Adding .repo files defined in recipe.yml --"
     for repo in "${repos[@]}"; do

9f46d9eac5d6b01b4c325be926618dc634529f7a
qoijjj <129108030+qoijjj@users.noreply.github.com>
2023-10-31 00:41:23 -0700

    Update example.sh

--- config/scripts/example.sh
+++ config/scripts/example.sh
@@ -8,3 +8,15 @@ set -oue pipefail
 # Your code goes here.
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
+
+
+echo "Setting more restrictive file permissions..."
+chmod 600 /etc/at.deny
+chmod 600 /etc/cron.deny
+chmod 600 /etc/crontab
+chmod 700 /etc/cron.d
+chmod 700 /etc/cron.daily/
+chmod 700 /etc/cron.daily
+chmod 700 /etc/cron.hourly
+chmod 700 /etc/cron.weekly
+chmod 700 /etc/cron.monthly

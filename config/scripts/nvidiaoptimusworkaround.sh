1568df0fadd46fc2e321687dfddd20792ea9a2a7
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-02-15 19:46:21 -0800

    fix: use improved workaround for nvidia optimus on gnome

--- config/scripts/nvidiaoptimusworkaround.sh
+++ config/scripts/nvidiaoptimusworkaround.sh
@@ -4,5 +4,5 @@
 set -oue pipefail
 
 # applies workaround for known issue with optimus laptops for GNOME and Cinnamon
-# https://gitlab.gnome.org/GNOME/mutter/-/issues/2969
-echo "__EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/50_mesa.json" >> /usr/etc/environment
+# https://gitlab.gnome.org/GNOME/mutter/-/issues/2969#note_1872558
+mv /usr/share/glvnd/egl_vendor.d/10_nvidia.json /usr/share/glvnd/egl_vendor.d/90_nvidia.json

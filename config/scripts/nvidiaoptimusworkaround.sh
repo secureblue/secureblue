6686d9ecc9189aec078c1713e14ff082a640b10a
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-03-11 18:04:13 -0700

    chore: remove deprecated images

--- config/scripts/nvidiaoptimusworkaround.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-# applies workaround for known issue with optimus laptops for GNOME and Cinnamon
-# https://gitlab.gnome.org/GNOME/mutter/-/issues/2969#note_1872558
-mv /usr/share/glvnd/egl_vendor.d/10_nvidia.json /usr/share/glvnd/egl_vendor.d/90_nvidia.json

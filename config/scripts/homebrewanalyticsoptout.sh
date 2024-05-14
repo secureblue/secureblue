9d6b7c60ff475c1dd9562cd8e693d9bae6abdf12
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-05-13 17:23:29 -0700

    feat: opt out of homebrew analytics by default

---
+++ config/scripts/homebrewanalyticsoptout.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+# https://docs.brew.sh/Analytics
+echo "
+
+HOMEBREW_NO_ANALYTICS=1
+
+" >> /usr/etc/environment

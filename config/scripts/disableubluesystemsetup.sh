3b40dc0b41ca48d30361dfb05960fb840cc26d08
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-05-23 12:56:18 -0700

    feat: disable all vscode telem by default for dx images

--- config/scripts/disableubluesystemsetup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-echo "Disabling ublue-system-setup"
-systemctl disable ublue-system-setup
-systemctl mask ublue-system-setup

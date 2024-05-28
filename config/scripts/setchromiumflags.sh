d34c8e5892508715239179412fdf98016837d72b
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-05-28 11:20:08 -0700

    fix: wrong quotes in script

--- config/scripts/setchromiumflags.sh
+++ config/scripts/setchromiumflags.sh
@@ -3,8 +3,8 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-echo "
+echo '
 
 CHROMIUM_FLAGS+=" --ozone-platform=wayland --no-pings --disk-cache-dir=/dev/null --enable-features=SplitCacheByNetworkIsolationKey,SplitCodeCacheByNetworkIsolationKey,SplitHostCacheByNetworkIsolationKey,PrefetchPrivacyChanges,IsolateSandboxedIframes,StrictOriginIsolation,PartitionConnectionsByNetworkIsolationKey,PartitionHttpServerPropertiesByNetworkIsolationKey,PartitionSSLSessionsByNetworkIsolationKey,PartitionNelAndReportingByNetworkIsolationKey,EnableCrossSiteFlagNetworkIsolationKey --disable-features=PrivacySandboxSettings4,InterestFeedV2,NTPPopularSitesBakedInContent,UsePopularSitesSuggestions,MediaDrmPreprovisioning,AutofillServerCommunication,DisableThirdPartyStoragePartitioningDeprecationTrial"
 
-" >> /usr/etc/chromium/chromium.conf
+' >> /usr/etc/chromium/chromium.conf

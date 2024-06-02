f38bf8818a6ba0df09850f4bc6eafd7ef7d670ec
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-06-01 22:37:14 -0700

    fix: point chromium config script at /etc

--- config/scripts/setchromiumflags.sh
+++ config/scripts/setchromiumflags.sh
@@ -3,11 +3,11 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/FEATURES=""/FEATURES="SplitCacheByNetworkIsolationKey,SplitCodeCacheByNetworkIsolationKey,SplitHostCacheByNetworkIsolationKey,PrefetchPrivacyChanges,IsolateSandboxedIframes,StrictOriginIsolation,PartitionConnectionsByNetworkIsolationKey,PartitionHttpServerPropertiesByNetworkIsolationKey,PartitionSSLSessionsByNetworkIsolationKey,PartitionNelAndReportingByNetworkIsolationKey,EnableCrossSiteFlagNetworkIsolationKey"/g' /usr/etc/chromium/chromium.conf
+sed -i 's/FEATURES=""/FEATURES="SplitCacheByNetworkIsolationKey,SplitCodeCacheByNetworkIsolationKey,SplitHostCacheByNetworkIsolationKey,PrefetchPrivacyChanges,IsolateSandboxedIframes,StrictOriginIsolation,PartitionConnectionsByNetworkIsolationKey,PartitionHttpServerPropertiesByNetworkIsolationKey,PartitionSSLSessionsByNetworkIsolationKey,PartitionNelAndReportingByNetworkIsolationKey,EnableCrossSiteFlagNetworkIsolationKey"/g' /etc/chromium/chromium.conf
 
 # Not temporary, keep:
 echo '
 
 CHROMIUM_FLAGS+=" --ozone-platform=wayland --no-pings --disk-cache-dir=/dev/null --disable-features=PrivacySandboxSettings4,InterestFeedV2,NTPPopularSitesBakedInContent,UsePopularSitesSuggestions,MediaDrmPreprovisioning,AutofillServerCommunication,DisableThirdPartyStoragePartitioningDeprecationTrial"
 
-' >> /usr/etc/chromium/chromium.conf
+' >> /etc/chromium/chromium.conf

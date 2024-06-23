e0db33333b53509eb145545b5c35ff97083c4369
Furkan Uysal <81188796+furkun@users.noreply.github.com>
2024-06-23 16:09:10 +0300

    Update configureusbguard.sh

--- config/scripts/configureusbguard.sh
+++ config/scripts/configureusbguard.sh
@@ -3,5 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sudo sed -i 's/AuditBackend=FileAudit/AuditBackend=LinuxAudit/' /etc/usbguard/usbguard-daemon.conf
-sudo chmod 600 /etc/usbguard/rules.conf
+sed -i 's/AuditBackend=FileAudit/AuditBackend=LinuxAudit/' /etc/usbguard/usbguard-daemon.conf
+chmod 600 /etc/usbguard/rules.conf

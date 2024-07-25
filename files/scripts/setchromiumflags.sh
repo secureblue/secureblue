c16debbdd4a8bd4825446a9b94e3d801c1a0b9e5
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-07-25 00:15:25 -0700

    Revert "feat: switch to hardened-chromium (#332)"
    This reverts commit 721ad757b50221b5a8756f075e1a4df5b5671c86.

---
+++ files/scripts/setchromiumflags.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+sed -i '/--enable-chrome-browser-cloud-management/d' /etc/chromium/chromium.conf
+
+# https://bugzilla.redhat.com/show_bug.cgi?id=2293202
+sed -i '/--enable-native-gpu-memory-buffers/d' /etc/chromium/chromium.conf
+
+sed -i 's/FEATURES=""/FEATURES="SplitCacheByNetworkIsolationKey,SplitCodeCacheByNetworkIsolationKey,SplitHostCacheByNetworkIsolationKey,PrefetchPrivacyChanges,IsolateSandboxedIframes,StrictOriginIsolation,PartitionConnectionsByNetworkIsolationKey,PartitionHttpServerPropertiesByNetworkIsolationKey,PartitionSSLSessionsByNetworkIsolationKey,PartitionNelAndReportingByNetworkIsolationKey,EnableCrossSiteFlagNetworkIsolationKey,ContentSettingsPartitioning,"/g' /etc/chromium/chromium.conf
+
+echo '
+
+CHROMIUM_FLAGS+=" --ozone-platform=wayland --js-flags=--jitless --no-pings --disk-cache-dir=/dev/null --extension-content-verification=enforce_strict --extensions-install-verification=enforce_strict --disable-features=PrivacySandboxSettings4,InterestFeedV2,NTPPopularSitesBakedInContent,UsePopularSitesSuggestions,MediaDrmPreprovisioning,AutofillServerCommunication,DisableThirdPartyStoragePartitioningDeprecationTrial,OptimizationHints,OptimizationHintsFetching,OptimizationHintsFetchingAnonymousDataConsent"
+
+' >> /etc/chromium/chromium.conf

9a843f3861928971988e6f8527337e16e3df64e7
Root <175176948+RKNF404@users.noreply.github.com>
2024-07-29 12:57:15 -0400

    docs: add docs to JIT disable in Gnome (#345)
    * Add docs to JIT disable in Gnome
    
    * Properly add env file in ujust

--- files/system/usr/etc/profile.d/gnome-disable-jit.sh
+++ files/system/usr/etc/profile.d/gnome-disable-jit.sh
@@ -1,2 +1,2 @@
-export JavaScriptCoreUseJIT=0
-export GJS_DISABLE_JIT=1
+export JavaScriptCoreUseJIT=0 # https://trac.webkit.org/wiki/EnvironmentVariables
+export GJS_DISABLE_JIT=1 # https://gitlab.gnome.org/GNOME/gjs/-/blob/master/doc/Environment.md

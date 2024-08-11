0893e0387002e417d9812b68d0066926ee3404d1
Ivo Damjanović <ivo@damjanovic.it>
2024-08-11 03:41:56 +0200

    fix: build workaround for cosmic

--- files/scripts/hardencontainerpolicy.sh
+++ files/scripts/hardencontainerpolicy.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/insecureAcceptAnything/reject/' /usr/etc/containers/policy.json
+sed -i 's/insecureAcceptAnything/reject/' /etc/containers/policy.json
 
 
 # Exception for build-container-installer to allow the ISO generation script to work
@@ -12,24 +12,24 @@ yq -i -o=j '.transports.docker |=
     {"ghcr.io/jasonn3": [
         {
           "type": "sigstoreSigned",
-          "keyPath": "/usr/etc/pki/containers/build-container-installer.pub",
+          "keyPath": "/etc/pki/containers/build-container-installer.pub",
           "signedIdentity": {
             "type": "matchRepository"
           }
         }
       ]
     }
-+ .' /usr/etc/containers/policy.json
++ .' /etc/containers/policy.json
 
 yq -i -o=j '.transports.docker |=
     {"ghcr.io/zelikos": [
         {
           "type": "sigstoreSigned",
-          "keyPath": "/usr/etc/pki/containers/davincibox.pub",
+          "keyPath": "/etc/pki/containers/davincibox.pub",
           "signedIdentity": {
             "type": "matchRepository"
           }
         }
       ]
     }
-+ .' /usr/etc/containers/policy.json
++ .' /etc/containers/policy.json

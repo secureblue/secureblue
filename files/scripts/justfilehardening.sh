4c04c11b9044eba13345e470d85d7a57d24ee834
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-08-18 20:21:41 -0700

    fix: typo in justfile script

--- files/scripts/justfilehardening.sh
+++ files/scripts/justfilehardening.sh
@@ -4,5 +4,5 @@
 set -oue pipefail
 
 patch /usr/share/ublue-os/just/05-brew.just < enable-wheelless-brew-installation.patch 
-patch /usr/share/ublue-os/just/15-brew.just < use-pkexec-in-luks-just.patch
+patch /usr/share/ublue-os/just/15-luks.just < use-pkexec-in-luks-just.patch
 patch /usr/libexec/luks-enable-tpm2-autounlock < improve-luks-tpm-script.patch 

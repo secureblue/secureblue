4a1dd61a31a124818703a41318960eb1004263b4
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-09-30 13:07:27 -0700

    fix: import brew justfile

---
+++ files/scripts/addbrewjustimport.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+echo "
+
+import "/usr/share/ublue-os/just/50-brew.just"
+
+" >> /usr/share/ublue-os/justfile

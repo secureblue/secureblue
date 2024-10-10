e104b5d8afc89b3185922355671a236bf880ce0a
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-10 13:31:22 -0700

    feat: initial testing of moving off of ublue

---
+++ files/scripts/installrpmfusion.sh
@@ -0,0 +1,3 @@
+  rpm-ostree install \
+      https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm \
+      https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm

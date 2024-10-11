861d0a41643eea154cc88644e7eac7ec703ea197
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-11 14:22:09 -0700

    add udev rules and signing steps

--- files/scripts/removeunusedrepos.sh
+++ files/scripts/removeunusedrepos.sh
@@ -7,3 +7,4 @@ rm /etc/yum.repos.d/negativo17-fedora-nvidia.repo
 rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 rm /etc/yum.repos.d/eyecantcu-supergfxctl.repo
 rm /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+rm /etc/yum.repos.d/nvidia-container-toolkit.repo

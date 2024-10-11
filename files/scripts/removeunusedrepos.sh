a1e2fa9244e13cc955452388ea8adfa182ac1cc9
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-11 13:08:56 -0700

    remove unused repos

---
+++ files/scripts/removeunusedrepos.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+rm /etc/yum.repos.d/negativo17-fedora-nvidia.repo
+rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+rm /etc/yum.repos.d/eyecantcu-supergfxctl.repo
+rm /etc/yum.repos.d/_copr_ublue-os-akmods.repo

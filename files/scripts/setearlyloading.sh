4e47ca6a2364addca588f907fc01269543ba1226
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-14 18:56:20 -0700

    fix script again

--- files/scripts/setearlyloading.sh
+++ files/scripts/setearlyloading.sh
@@ -6,7 +6,8 @@ set -oue pipefail
 sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
 sed -i 's@ nvidia @ i915 amdgpu nvidia @g' /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
 
-rpm-ostree cliwrap install-to-root
+rpm-ostree cliwrap install-to-root /
+
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"

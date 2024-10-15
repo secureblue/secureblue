a5363ad627bbc567b50598b381f0e48e6ca8ae9a
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-14 18:47:40 -0700

    fix script

--- files/scripts/setearlyloading.sh
+++ files/scripts/setearlyloading.sh
@@ -3,18 +3,10 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-KERNEL_SUFFIX=""
-
 sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
-sed -i 's@nvidia-uvm@nvidia-uvm i915 amdgpu@g' /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
+sed -i 's@ nvidia @ i915 amdgpu nvidia @g' /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
 
-
-if [ ! -f /usr/libexec/rpm-ostree/wrapped/dracut ]; then
-    rpm-ostree cliwrap install-to-root /
-fi
-
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
-
+rpm-ostree cliwrap install-to-root
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"

1207b96b1e7ee86a9f52785975013ace41683dc9
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-17 15:36:57 -0700

    script fixes

--- files/scripts/installsignedkernel.sh
+++ files/scripts/installsignedkernel.sh
@@ -7,20 +7,10 @@ find /tmp/rpms
 
 rpm-ostree cliwrap install-to-root /
 
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
-
-if [[ "${KERNEL_VERSION}" == "${QUALIFIED_KERNEL}" ]]; then
-    echo "Installing signed kernel from kernel-cache."
-    cd /tmp
-    rpm2cpio /tmp/rpms/kernel/kernel-core-*.rpm | cpio -idmv
-    cp ./lib/modules/*/vmlinuz /usr/lib/modules/*/vmlinuz
-    cd /
-else
-    echo "Install kernel version ${KERNEL_VERSION} from kernel-cache."
-    rpm-ostree override replace \
-        --experimental \
-        --install=zstd \
-        /tmp/rpms/kernel/kernel-[0-9]*.rpm \
-        /tmp/rpms/kernel/kernel-core-*.rpm \
-        /tmp/rpms/kernel/kernel-modules-*.rpm
-fi
+echo "Install kernel from kernel-cache."
+rpm-ostree override replace \
+    --experimental \
+    --install=zstd \
+    /tmp/rpms/kernel/kernel-[0-9]*.rpm \
+    /tmp/rpms/kernel/kernel-core-*.rpm \
+    /tmp/rpms/kernel/kernel-modules-*.rpm

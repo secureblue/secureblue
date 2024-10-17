3517c8e412d3f54d4a77fa4fb7d621a59ca5c932
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-17 16:02:26 -0700

    pull kernel version properly

--- files/scripts/installsignedkernel.sh
+++ files/scripts/installsignedkernel.sh
@@ -7,10 +7,21 @@ find /tmp/rpms
 
 rpm-ostree cliwrap install-to-root /
 
-echo "Install kernel from kernel-cache."
-rpm-ostree override replace \
-    --experimental \
-    --install=zstd \
-    /tmp/rpms/kernel/kernel-[0-9]*.rpm \
-    /tmp/rpms/kernel/kernel-core-*.rpm \
-    /tmp/rpms/kernel/kernel-modules-*.rpm
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
+INCOMING_KERNEL_VERSION="$(basename -s .fc40.x86_64.rpm $(ls /tmp/rpms/kernel/kernel-[0-9]*.rpm 2>/dev/null | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//'))"
+
+echo "Qualified kernel: $QUALIFIED_KERNEL"
+echo "Incoming kernel version: $INCOMING_KERNEL_VERSION"
+
+
+if [[ "$FIRST_KERNEL_VERSION" != "$QUALIFIED_KERNEL" ]]; then
+    echo "Installing kernel from kernel-cache."
+    rpm-ostree override replace \
+        --experimental \
+        --install=zstd \
+        /tmp/rpms/kernel/kernel-[0-9]*.rpm \
+        /tmp/rpms/kernel/kernel-core-*.rpm \
+        /tmp/rpms/kernel/kernel-modules-*.rpm
+else
+    echo "Skipping installation: version matches installed kernel ($QUALIFIED_KERNEL)"
+fi

002465d0699a08195bb436e2be6d7cbacf8633fc
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-02 21:42:05 -0800

    feat: SELinux: prevent unconfined relabelling to userns-permissive types

--- files/scripts/installselinuxpolicies.sh
+++ files/scripts/installselinuxpolicies.sh
@@ -21,3 +21,10 @@ semodule -i ./selinux/user_namespace/grant_userns.cil
 semodule -i ./selinux/user_namespace/harden_userns.cil
 semodule -i ./selinux/user_namespace/harden_container_userns.cil
 semodule -i ./selinux/flatpakfull/grant_systemd_flatpak_exec.cil
+
+semodule -i ./selinux/user_namespace/deny_unconfined_blk_file_relabels.cil
+semodule -i ./selinux/user_namespace/deny_unconfined_chr_file_relabels.cil
+semodule -i ./selinux/user_namespace/deny_unconfined_dir_relabels.cil
+semodule -i ./selinux/user_namespace/deny_unconfined_fifo_file_relabels.cil
+semodule -i ./selinux/user_namespace/deny_unconfined_file_relabels.cil
+semodule -i ./selinux/user_namespace/deny_unconfined_lnk_file_relabels.cil

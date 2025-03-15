c81f77006d9e846a6b34bf252f0c9514368b902e
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-14 20:27:08 -0700

    testing: restructure nvidia

---
+++ files/scripts/installnvidiapackages.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+
+
+find /tmp/rpms
+
+
+nvidia_packages_list="/tmp/rpms/kmods/kmod-nvidia*.rpm nvidia-container-toolkit nvidia-driver-cuda"
+
+if [[ "$IMAGE_NAME" == *"securecore"* ]]; then
+  nvidia_config_rpm_location="/tmp/rpms/ucore/ublue-os-ucore-nvidia*.rpm"
+else
+  nvidia_config_rpm_location="/tmp/rpms/ublue-os/ublue-os-nvidia*.rpm"
+  nvidia_packages_list+="libnvidia-fbc libva-nvidia-driver nvidia-driver nvidia-modprobe nvidia-persistenced nvidia-settings"
+fi
+
+if [ ! -f /etc/yum.repos.d/negativo17-fedora-nvidia.repo ]; then
+  curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/negativo17-fedora-nvidia.repo
+fi
+
+rpm-ostree install $nvidia_config_rpm_location
+sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/nvidia-container-toolkit.repo
+sed -i '0,/enabled=0/{s/enabled=0/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-nvidia.repo  
+rpm-ostree install $nvidia_packages_list

e573d95b8cf271fb845131e347359881c44b3f39
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-16 23:39:03 -0700

    codacy

--- files/scripts/installnvidiapackages.sh
+++ files/scripts/installnvidiapackages.sh
@@ -18,10 +18,10 @@ if [ ! -f /etc/yum.repos.d/negativo17-fedora-nvidia.repo ]; then
   curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/negativo17-fedora-nvidia.repo
 fi
 
-rpm-ostree install $nvidia_config_rpm_location
+rpm-ostree install "$nvidia_config_rpm_location"
 sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/nvidia-container-toolkit.repo
 sed -i '0,/enabled=0/{s/enabled=0/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-nvidia.repo  
-rpm-ostree install $nvidia_packages_list
+rpm-ostree install "$nvidia_packages_list"
 
 kmod_version=$(ls /tmp/rpms/kmods/kmod-nvidia*.rpm | awk -F'-' '{print $(NF-1)}')
 negativo_version=$(rpm -qa | grep nvidia-modprobe | awk -F':' '{print $(NF)}' | awk -F'-' '{print $(NF-1)}')

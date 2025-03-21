7160d73431b71006a97a94cf221735ed5bb5afe3
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-20 21:43:11 -0700

    fix

--- files/scripts/installnvidiapackages.sh
+++ files/scripts/installnvidiapackages.sh
@@ -18,7 +18,7 @@ if [ ! -f /etc/yum.repos.d/negativo17-fedora-nvidia.repo ]; then
   curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/negativo17-fedora-nvidia.repo
 fi
 
-rpm-ostree install "$nvidia_config_rpm_location"
+rpm-ostree install $nvidia_config_rpm_location
 sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/nvidia-container-toolkit.repo
 sed -i '0,/enabled=0/{s/enabled=0/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-nvidia.repo  
 rpm-ostree install "${nvidia_packages_list[@]}"

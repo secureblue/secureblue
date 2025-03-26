5d352b3043d4fca60ecc50b69228f3f36dd45a6a
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-25 19:51:39 -0700

    install pubkey

--- files/scripts/signkernel.sh
+++ files/scripts/signkernel.sh
@@ -13,3 +13,5 @@ PRIVATE_KEY_PATH="./certs/private_key.priv"
 openssl x509 -in "$PUBLIC_KEY_DER_PATH" -out "$PUBLIC_KEY_CRT_PATH"
 sbsign --cert "$PUBLIC_KEY_CRT_PATH" --key "$PRIVATE_KEY_PATH" /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz --output /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
 sbverify --list /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
+
+install -Dm0644 $PUBLIC_KEY_DER_PATH /etc/pki/akmods/certs/akmods-secureblue.der

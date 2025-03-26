56e9e86f8b8d1c9ce828a9cc2190bdf3cbcb470f
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-25 19:23:38 -0700

    fix

--- files/scripts/installnvidiapackages.sh
+++ files/scripts/installnvidiapackages.sh
@@ -10,7 +10,7 @@ fi
 
 curl -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo \
     -o /etc/yum.repos.d/nvidia-container-toolkit.repo
-sed -i "s@gpgcheck=0@gpgcheck=1@" /tmp/ublue-os-ucore-nvidia/rpmbuild/SOURCES/nvidia-container-toolkit.repo
+sed -i "s@gpgcheck=0@gpgcheck=1@" /etc/yum.repos.d/nvidia-container-toolkit.repo
 
 curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/negativo17-fedora-nvidia.repo
 

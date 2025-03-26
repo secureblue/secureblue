b96020f5b61c843ce06fae4e29b10a1a90a2732a
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-26 00:17:46 -0700

    fix

--- files/scripts/installzfskmod.sh
+++ files/scripts/installzfskmod.sh
@@ -54,7 +54,7 @@ cd zfs-${ZFS_VERSION}
     && make -j $(nproc) rpm-utils rpm-kmod \
     || (cat config.log && exit 1)
 
-dnf install -y ./kmod-zfs-*.rpm ./zfs-kmod-common-*.rpm
+dnf install -y ./*.rpm
 cd ..
 
 ./signmodules.sh "zfs"

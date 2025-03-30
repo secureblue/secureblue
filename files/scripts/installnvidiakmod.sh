8fdd40fac7992c50ae9925fca23a86e14711980f
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-29 19:03:59 -0700

    codacy

--- files/scripts/installnvidiakmod.sh
+++ files/scripts/installnvidiakmod.sh
@@ -26,12 +26,8 @@ fi
 curl -Lo /etc/yum.repos.d/negativo17-fedora-nvidia.repo https://negativo17.org/repos/fedora-nvidia.repo
 sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-nvidia.repo
 
-
 dnf install -y "kernel-devel-matched-$(rpm -q 'kernel' --queryformat '%{VERSION}')"
 dnf install -y "akmod-nvidia*.fc${RELEASE}"
-
-
-NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
 
 sed -i "s/^MODULE_VARIANT=.*/MODULE_VARIANT=$KERNEL_MODULE_TYPE/" /etc/nvidia/kernel.conf
 akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"

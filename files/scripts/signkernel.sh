5ac66e8ad38f6a7c82ec1ce0fd7a3f19cd6eb345
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-29 18:33:28 -0700

    fix

--- files/scripts/signkernel.sh
+++ files/scripts/signkernel.sh
@@ -5,12 +5,10 @@ set -oue pipefail
 
 KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-PUBLIC_KEY_DER_PATH="./certs/public_key.der"
+PUBLIC_KEY_DER_PATH="../system/etc/pki/akmods/certs/akmods-secureblue.der"
 PUBLIC_KEY_CRT_PATH="./certs/public_key.crt"
 PRIVATE_KEY_PATH="./certs/private_key.priv"
 
 openssl x509 -in "$PUBLIC_KEY_DER_PATH" -out "$PUBLIC_KEY_CRT_PATH"
 sbsign --cert "$PUBLIC_KEY_CRT_PATH" --key "$PRIVATE_KEY_PATH" /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz --output /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
 sbverify --list /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
-
-install -Dm0644 $PUBLIC_KEY_DER_PATH /etc/pki/akmods/certs/akmods-secureblue.der

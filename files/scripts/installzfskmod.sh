978a576c775bbac0ae89fed532dde1e7f501d6a5
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-03-31 09:38:53 -0700

    fix

--- files/scripts/installzfskmod.sh
+++ files/scripts/installzfskmod.sh
@@ -83,6 +83,5 @@ omit_dracutmodules+=" zfs "
 
 depmod -a -v "${KERNEL_VERSION}"
 
-dnf remove -y "kernel-devel-matched-$(rpm -q 'kernel' --queryformat '%{VERSION}')"
-dnf remove -y autoconf automake gcc pv akmods mock libtirpc-devel libblkid-devel libuuid-devel libudev-devel openssl-devel libaio-devel libattr-devel elfutils-libelf-devel python3-devel libffi-devel libcurl-devel ncompress python3-setuptools
+dnf remove -y autoconf automake gcc pv sudo akmods mock libtirpc-devel libblkid-devel libuuid-devel libudev-devel openssl-devel libaio-devel libattr-devel elfutils-libelf-devel python3-devel libffi-devel libcurl-devel ncompress python3-setuptools
 dnf autoremove -y

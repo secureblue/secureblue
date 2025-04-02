fa261a3e94ca67df3709ff5d8a02d0b7bd1abcf0
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-04-02 07:37:04 -0700

    fix: include libheif dep before installing it

--- files/scripts/installproprietarypackages.sh
+++ files/scripts/installproprietarypackages.sh
@@ -6,6 +6,8 @@ set -oue pipefail
 curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
+rpm-ostree install libopenjph
+
 rpm-ostree override replace \
   --experimental \
   --from repo='fedora-multimedia' \

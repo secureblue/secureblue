1b5c290eb502c2973207b38d58485831a34c1047
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-04-08 06:59:13 -0700

    fix: array expansion

--- files/scripts/removesudo.sh
+++ files/scripts/removesudo.sh
@@ -8,6 +8,6 @@ if [[ "$IMAGE_NAME" != *"kinoite"* ]]; then
     SUDO_PACKAGES_TO_REMOVE+=('sudo')
 fi
 
-rpm-ostree override remove "$SUDO_PACKAGES_TO_REMOVE"
+rpm-ostree override remove "${SUDO_PACKAGES_TO_REMOVE[@]}"
 
 rm -rf /usr/bin/sudo

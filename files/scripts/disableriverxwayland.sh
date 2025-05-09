e93aea8da83014ad254a338af3177eb4aabedc78
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-05-09 15:46:40 -0700

    remove no longer relevant files

--- files/scripts/disableriverxwayland.sh
+++
@@ -1,8 +0,0 @@
-
-
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-sed -i 's@XDG_CONFIG_HOME=/usr/etc/ river@XDG_CONFIG_HOME=/usr/etc/ river -no-xwayland@g' /usr/bin/startriver

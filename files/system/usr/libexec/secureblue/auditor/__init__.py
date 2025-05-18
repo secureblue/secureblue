97a54ab3d61cea40b3869d75a505c0a4c8e4a909
Daniel Hast <hast.daniel@protonmail.com>
2025-05-17 20:23:17 -0400

    improve visuals of line between report description and status

--- files/system/usr/libexec/secureblue/auditor/__init__.py
+++ files/system/usr/libexec/secureblue/auditor/__init__.py
@@ -74,7 +74,10 @@ class Report:
     def to_str(self, width: int = 80) -> str:
         status_tag = f" [ {self.status.to_str_in_color()} ]"
         desc_width = width - len(self.status.name) - 5
-        report_str = f"{(self.description + '...').ljust(desc_width, '.')}{status_tag}"
+        gray_start = "\x1b[38;5;241m"
+        reset_color = "\x1b[39m"
+        desc_with_sep = f"{self.description}... {gray_start}".ljust(desc_width, "…") + reset_color
+        report_str = desc_with_sep + status_tag
         for warning in self.warnings:
             report_str += f"\n> {warning}"
         return report_str

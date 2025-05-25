a25efd92540d19ac8d38b7ae159ad5353ac19429
Daniel Hast <hast.daniel@protonmail.com>
2025-05-25 09:41:46 -0400

    fix(audit): only check permissions of flatpak apps, not runtimes
    Flatpak overrides on runtimes don't appear to impact the actual
    permissions of flatpak apps using that runtime, so there's no point in
    checking them. The behavior of the flatpak sandbox is determined by the
    permissions on the app itself, which are already checked.
    
    Signed-off-by: Daniel Hast <hast.daniel@protonmail.com>

--- files/system/usr/libexec/secureblue/audit_secureblue.py
+++ files/system/usr/libexec/secureblue/audit_secureblue.py
@@ -781,7 +781,9 @@ async def audit_flatpak_permissions(state):
         return
 
     flatpaks = []
-    for line in command_stdout(*"flatpak list --columns=application,branch".split()).split("\n"):
+    for line in command_stdout(*"flatpak list --app --columns=application,branch".split()).split(
+        "\n"
+    ):
         if not line:
             continue
         name, version = line.split("\t")
@@ -799,7 +801,11 @@ async def audit_flatpak_permissions(state):
         status, warnings, recs = check_flatpak_permissions(
             name, perms, state["bluetooth_loaded"], state["ptrace_allowed"]
         )
-        yield Report(f"Auditing {name} ({version})", status, warnings=warnings, recs=recs)
+        if version == "stable":
+            report_text = f"Auditing {name}"
+        else:
+            report_text = f"Auditing {name} ({version})"
+        yield Report(report_text, status, warnings=warnings, recs=recs)
 
 
 ###############################################################################

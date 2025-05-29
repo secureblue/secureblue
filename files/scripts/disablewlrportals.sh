6cbe16e762c70c87f5196eca0dd648cadf796473
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-05-29 10:15:33 -0700

    Update disablewlrportals.sh

--- files/scripts/disablewlrportals.sh
+++ files/scripts/disablewlrportals.sh
@@ -4,5 +4,4 @@ set -oue pipefail
 
 PORTALS_CONF="/usr/share/xdg-desktop-portal/sway-portals.conf"
 
-sed -i "s/org\.freedesktop\.impl\.portal\.ScreenCast=wlr/org.freedesktop.impl.portal.ScreenCast=none/" "$PORTALS_CONF"
-sed -i "s/org\.freedesktop\.impl\.portal\.Screenshot=wlr/org.freedesktop.impl.portal.Screenshot=none/" "$PORTALS_CONF"
+sed -Ei "/^org\.freedesktop\.impl\.portal\.Screen(Cast|shot)=wlr$/s/=wlr/=none/" "$PORTALS_CONF"

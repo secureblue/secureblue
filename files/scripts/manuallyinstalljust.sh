a3875805b92404dff090d786e66a6695530eca39
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-05-29 11:28:00 -0700

    codacy

--- files/scripts/manuallyinstalljust.sh
+++ files/scripts/manuallyinstalljust.sh
@@ -14,8 +14,8 @@ set -oue pipefail
 # Unless required by applicable law or agreed to in writing, software distributed under the License is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
-
-VER="$(basename $(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/casey/just/releases/latest))"
+LATEST_URL="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/casey/just/releases/latest)"
+VER="$(basename $LATEST_URL)"
 curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/just-${VER}-x86_64-unknown-linux-musl.tar.gz" -o "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
 curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/SHA256SUMS" -o /tmp/SHA256SUMS
 cd /tmp

688eb7c0d4fcc16b109ace045cb83318574ae96e
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-06-22 08:49:09 -0700

    more fixes

--- files/scripts/setdefaultkdewallpapers.sh
+++ files/scripts/setdefaultkdewallpapers.sh
@@ -14,8 +14,8 @@
 
 set -oue pipefail
 
-ln -sf /usr/share/backgrounds/secureblue/secureblue-blue.svg /usr/share/backgrounds/default.png
-ln -sf /usr/share/backgrounds/secureblue/secureblue-blue.svg /usr/share/backgrounds/default-dark.png
+ln -sf /usr/share/backgrounds/secureblue/secureblue-blue.png /usr/share/backgrounds/default.png
+ln -sf /usr/share/backgrounds/secureblue/secureblue-black.png /usr/share/backgrounds/default-dark.png
 
 ln -sf /usr/share/backgrounds/default.png /usr/share/backgrounds/default.jxl
 ln -sf /usr/share/backgrounds/default-dark.png /usr/share/backgrounds/default-dark.jxl

53e76a5fccda5eb73d4600b9ec4deaaba2a3d48c
josh3kd <216279332+josh3kd@users.noreply.github.com>
2025-07-15 15:11:15 +0800

    Update disablecups.sh

--- files/scripts/disablecups.sh
+++ files/scripts/disablecups.sh
@@ -15,8 +15,10 @@
 set -oue pipefail
 
 echo "Disabling print services"
-systemctl disable cups
-systemctl mask cups
+systemctl disable cups.socket
+systemctl mask cups.socket
+systemctl disable cups.service
+systemctl mask cups.service
 
 systemctl disable cups-browsed
 systemctl mask cups-browsed

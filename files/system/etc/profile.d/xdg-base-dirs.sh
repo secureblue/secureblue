d099e47a1cdbdd7d2ed1cf317d8f296597dd4762
Daniel Hast <hast.daniel@protonmail.com>
2025-07-22 08:27:45 -0400

    fix: set XDG_CONFIG_HOME explicitly
    This avoids podman checking whether `~/.config` is owned by the current
    user and erroring out if it doesn't. Unclear how long this can be relied
    on but it works for now and in any case is harmless since we're just
    setting these to their defaults.
    
    Also set the other xdg base directories to their default values for
    consistency.

---
+++ files/system/etc/profile.d/xdg-base-dirs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+# For reference, see: https://specifications.freedesktop.org/basedir-spec/latest/
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_STATE_HOME="${HOME}/.local/state"
+export XDG_CACHE_HOME="${HOME}/.cache"

e2d0aeccabaaefcac1ebf22c799f4d82aef5475e
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-08-01 16:26:34 -0700

    fix: tests

--- .github/workflows/integration_tests/check_firstrun.sh
+++ .github/workflows/integration_tests/check_firstrun.sh
@@ -4,9 +4,13 @@ SERVICE_NAME="securebluefirstrun.service"
 if ! systemctl is-enabled --quiet "$SERVICE_NAME"; then
     echo "Error: $SERVICE_NAME is in a disabled state."
     exit 1
+else 
+    echo "$SERVICE_NAME is enabled."
 fi
 
 if systemctl is-failed --quiet "$SERVICE_NAME"; then
     echo "Error: $SERVICE_NAME is in a failed state."
     exit 1
+else
+    echo "$SERVICE_NAME succeeded."
 fi

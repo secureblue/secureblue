f607ca5d3dfe6623a3258f1f6994d48de9f1bba4
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-08-25 14:28:41 -0700

    fix

--- files/system/usr/libexec/secureblue/audit_secureblue.py
+++ files/system/usr/libexec/secureblue/audit_secureblue.py
@@ -946,6 +946,9 @@ def audit_webcam_module():
             "$ ujust disable-webcam",
         )
         rec = "\n".join(rec_lines)
+    else
+        rec = None
+        status = UNKNOWN
     yield Report(_("Ensure Webcam module is disabled"), status, recs=rec)
 
 ###############################################################################

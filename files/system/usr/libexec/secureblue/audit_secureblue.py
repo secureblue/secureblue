7dfcf5334e582bbf44fdcaae1d0045bd22dd2bc0
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-08-25 14:36:46 -0700

    codacy

--- files/system/usr/libexec/secureblue/audit_secureblue.py
+++ files/system/usr/libexec/secureblue/audit_secureblue.py
@@ -863,6 +863,7 @@ def audit_webcam_module():
     """Ensure Webcam module is disabled."""
     webcam_mod_file = "/etc/modprobe.d/99-disable-webcam.conf"
     status = UNKNOWN
+    warning = None
     try:
         with open(webcam_mod_file, encoding="utf-8") as f:
             conf = f.readlines()
@@ -881,8 +882,8 @@ def audit_webcam_module():
             "$ ujust disable-webcam",
         )
         rec = "\n".join(rec_lines)
-        
-    yield Report(_("Ensure Webcam module is disabled"), status, recs=rec)
+
+    yield Report(_("Ensure Webcam module is disabled"), status, warnings=warning, recs=rec)
 
 
 @audit

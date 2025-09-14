08a19485c4926ac83f8a239da33d92e17b14a333
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-09-14 09:00:04 -0700

    Update ujust-chooser.sh

--- files/system/etc/profile.d/ujust-chooser.sh
+++ files/system/etc/profile.d/ujust-chooser.sh
@@ -2,7 +2,7 @@
 
 if command -v fzf &> /dev/null
 then
-    export JUST_CHOOSER="fzf --multi --preview 'just --unstable --color always --justfile \"/usr/share/ublue-os/justfile\" --show {}'"
+    unset JUST_CHOOSER
 else
     export JUST_CHOOSER=/usr/bin/echo
 fi

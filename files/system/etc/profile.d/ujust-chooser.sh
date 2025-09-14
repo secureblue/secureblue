cb06ac564f1c58b501fe319fa34cb8f06d60d9fd
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-09-14 08:45:40 -0700

    fix: JUST_CHOOSER should preview commands

--- files/system/etc/profile.d/ujust-chooser.sh
+++ files/system/etc/profile.d/ujust-chooser.sh
@@ -2,7 +2,7 @@
 
 if command -v fzf &> /dev/null
 then
-    export JUST_CHOOSER="fzf"
+    export JUST_CHOOSER="fzf --multi --preview 'just --unstable --color always --justfile \"/usr/share/ublue-os/justfile\" --show {}'"
 else
     export JUST_CHOOSER=/usr/bin/echo
 fi

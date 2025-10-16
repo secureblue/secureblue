a1dbe58425d75394d8c7a4ae2761d1063c4fd4e0
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-15 18:46:29 -0700

    fix

--- files/scripts/installtrivalent.sh
+++ files/scripts/installtrivalent.sh
@@ -23,9 +23,10 @@ curl -Lo /etc/yum.repos.d/repo.secureblue.dev.secureblue.repo https://repo.secur
 # dnf4 must be used here due to https://github.com/rpm-software-management/dnf5/issues/1985
 dnf4 install --repoid=secureblue --downloadonly --best --downloaddir=. -y trivalent
 
-trivalent_rpm=$(find . -maxdepth 1 -type f -name "trivalent-*.${ARCH}.rpm")
+trivalent_rpm_search=$(find . -maxdepth 1 -type f -name "trivalent-*.${ARCH}.rpm")
+trivalent_rpm=${trivalent_rpm_search#./}
 trivalent_rpms_found=$(echo "$trivalent_rpm" | wc -l)
-trivalent_rpm_sans_suffix=${trivalent_rpm#./trivalent-}
+trivalent_rpm_sans_suffix=${trivalent_rpm#trivalent-}
 trivalent_version=${trivalent_rpm_sans_suffix%.${ARCH}.rpm}
 
 if [ "$trivalent_rpms_found" -eq 1 ]; then

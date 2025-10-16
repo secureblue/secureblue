17e379f46f920e593da76104f15684f3dab0e777
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-16 10:19:13 -0700

    test

--- files/scripts/installcommonpackages.sh
+++ files/scripts/installcommonpackages.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 Universal Blue
 # Copyright 2025 The Secureblue Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,17 +13,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-type: script
-scripts:
-  - installcommonpackages.sh
-  - manuallyinstalljust.sh
-  - hardenlogindefs.sh
-  - hardened_malloc-pam.sh
-  - authselect.sh
-  - setfilepermissions.sh
-  - createmissingdirectories.sh
-  - disablegeoclue.sh
-  - createjustcompletions.sh
-  - removefedoraflatpakremoteservice.sh
-  - localization.sh
-  - addimageinfo.sh
+set -oue pipefail
+
+
+rpm-ostree install rsync

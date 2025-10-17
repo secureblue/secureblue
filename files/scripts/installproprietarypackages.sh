3d850b487e52c99ec3bc45362867f14c08a5e1ca
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-17 09:59:06 -0700

    fix

--- files/scripts/installproprietarypackages.sh
+++ files/scripts/installproprietarypackages.sh
@@ -33,7 +33,6 @@ rpm-ostree override replace \
     mesa-libEGL \
     mesa-libGL \
     mesa-libgbm \
-    mesa-libxatracker \
     mesa-va-drivers \
     mesa-vulkan-drivers \
     gstreamer1-plugin-libav \

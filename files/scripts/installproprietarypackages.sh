fe44e29002230f0584eab2634ad17a0cd38a197b
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-17 10:18:33 -0700

    fix

--- files/scripts/installproprietarypackages.sh
+++ files/scripts/installproprietarypackages.sh
@@ -17,10 +17,7 @@ set -oue pipefail
 # Install from Fedora
 rpm-ostree install libopenjph
 
-curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '/^enabled=1/a\priority=90' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-
-rpm-ostree refresh-md
 
 rpm-ostree override replace \
   --experimental \

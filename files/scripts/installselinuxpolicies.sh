ca76ff149446f59443e23faa90abaa9fae26fe19
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-23 15:03:45 -0700

    fix: allow glycin to run sandboxed for thunar (#1514)

--- files/scripts/installselinuxpolicies.sh
+++ files/scripts/installselinuxpolicies.sh
@@ -16,9 +16,10 @@ set -oue pipefail
 
 dnf install -y --setopt=install_weak_deps=False selinux-policy-devel
 
-policy_modules=(trivalent flatpakfull nautilus systemsettings)
+policy_modules=(trivalent flatpakfull nautilus systemsettings thunar)
 
 cil_policy_modules=(
+    './selinux/user_namespace/grant_fm_userns.cil'
     './selinux/user_namespace/grant_userns.cil'
     './selinux/user_namespace/harden_userns.cil'
     './selinux/user_namespace/harden_container_userns.cil'

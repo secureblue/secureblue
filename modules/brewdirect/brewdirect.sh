a84e0c2f2e73de189ddd534ad8191a17b176c26b
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-11-08 15:19:16 -0800

    fix

--- modules/brewdirect/brewdirect.sh
+++ modules/brewdirect/brewdirect.sh
@@ -257,8 +257,8 @@ fi
 
 # Copy shell configuration files
 echo "Copying Brew bash & fish shell completions"
-cp -r "${MODULE_DIRECTORY}"/brew/brew-fish-completions.fish /usr/share/fish/vendor_conf.d/brew-fish-completions.fish
-cp -r "${MODULE_DIRECTORY}"/brew/brew-bash-completions.sh /etc/profile.d/brew-bash-completions.sh
+cp -r "${MODULE_DIRECTORY}"/brewdirect/brew-fish-completions.fish /usr/share/fish/vendor_conf.d/brew-fish-completions.fish
+cp -r "${MODULE_DIRECTORY}"/brewdirect/brew-bash-completions.sh /etc/profile.d/brew-bash-completions.sh
 
 # Register path symlink
 # We do this via tmpfiles.d so that it is created by the live system.

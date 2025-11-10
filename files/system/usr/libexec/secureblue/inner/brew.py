14b741ae30b3a973f680980f2c5e779abdcb8525
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-11-10 00:03:59 -0800

    WIP

---
+++ files/system/usr/libexec/secureblue/inner/brew.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+# Copyright 2025 The Secureblue Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The sandboxed brew disable function
+"""
+
+import os
+import sys
+import shutil
+from typing import Final
+
+LINUXBREW_DIR: Final[str] = "/home/linuxbrew/.linuxbrew/"
+BREW_PROFILE_FILE: Final[str] = "/etc/profile.d/brew.sh"
+
+
+def main() -> int:
+    """Enable or disable brew"""
+
+    required_args_count = 2
+    if len(sys.argv) != required_args_count:
+        return 1
+
+    mode = sys.argv[1]
+    match mode:
+        case "on":
+            shutil.copy(f"/usr{BREW_PROFILE_FILE}", BREW_PROFILE_FILE)
+            systemctl = subprocess.run(  # nosec
+                ["/usr/bin/systemctl", "enable", "brew-setup.service"],
+                check=False,
+                capture_output=True,
+            )
+        case "off":
+            shutil.rmtree(LINUXBREW_DIR, ignore_errors=False)
+            os.remove(BREW_PROFILE_FILE)
+            systemctl = subprocess.run(  # nosec
+                ["/usr/bin/systemctl", "disable", "brew-setup.service"],
+                check=False,
+                capture_output=True,
+            )
+
+if __name__ == "__main__":
+    sys.exit(main())

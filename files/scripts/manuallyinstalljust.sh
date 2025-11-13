90b52ce476ac71b5bf821f4c021c480fdbe96655
Daniel Hast <hast.daniel@protonmail.com>
2025-11-13 15:17:27 -0500

    chore: use manuallyinstalljust.sh in tests
    This ensures the version of `just` used in CI is the same as the one
    used in the images. Also made some minor reliability improvements to
    `manuallyinstalljust.sh`: use `mktemp` to generate a temporary directory
    instead of just using `/tmp`, and set `curl` options to retry on failure
    and to not suppress error message output.

--- files/scripts/manuallyinstalljust.sh
+++ files/scripts/manuallyinstalljust.sh
@@ -14,20 +14,23 @@ set -oue pipefail
 # Unless required by applicable law or agreed to in writing, software distributed under the License is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
-LATEST_URL="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/casey/just/releases/latest)"
-VER="$(basename "$LATEST_URL")"
-curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/just-${VER}-x86_64-unknown-linux-musl.tar.gz" -o "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
-curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/SHA256SUMS" -o /tmp/SHA256SUMS
-cd /tmp
-if ! sha256sum -c SHA256SUMS --ignore-missing
-then
+
+latest_url=$(curl -fLsS --retry 5 -o /dev/null -w '%{url_effective}' https://github.com/casey/just/releases/latest)
+ver=$(basename "$latest_url")
+temp_dir=$(mktemp -d)
+curl -fLsS --retry 5 --create-dirs \
+    "https://github.com/casey/just/releases/download/${ver}/just-${ver}-x86_64-unknown-linux-musl.tar.gz" -o "${temp_dir}/just-${ver}-x86_64-unknown-linux-musl.tar.gz" \
+    "https://github.com/casey/just/releases/download/${ver}/SHA256SUMS" -o "${temp_dir}/SHA256SUMS"
+cd "${temp_dir}"
+if ! sha256sum -c SHA256SUMS --ignore-missing; then
     echo "Just tarball verification FAILED! Exiting..."
     exit 1
 fi
 cd -
-mkdir -p /tmp/just && tar -xzf "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz" -C /tmp/just/
-cp /tmp/just/just /usr/bin/just && chmod 0755 /usr/bin/just
-cp /tmp/just/completions/just.bash /usr/share/bash-completion/completions/just
-cp /tmp/just/just.1 /usr/share/man/man1/just.1
-rm "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
-rm -r /tmp/just/
+mkdir "${temp_dir}/just"
+tar -xzf "${temp_dir}/just-${ver}-x86_64-unknown-linux-musl.tar.gz" -C "${temp_dir}/just/"
+cp "${temp_dir}/just/just" /usr/bin/just
+chmod 0755 /usr/bin/just
+cp "${temp_dir}/just/completions/just.bash" /usr/share/bash-completion/completions/just
+cp "${temp_dir}/just/just.1" /usr/share/man/man1/just.1
+rm -r "${temp_dir}"

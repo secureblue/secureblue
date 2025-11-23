d232e7443d76a969138b30537012bb05eb99e0cb
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-11-23 13:51:48 -0800

    feat: switch to selinux-policy rpm

--- files/scripts/installselinuxpolicies.sh
+++ files/scripts/installselinuxpolicies.sh
@@ -14,25 +14,8 @@
 
 set -oue pipefail
 
-dnf install -y --setopt=install_weak_deps=False selinux-policy-devel
+cil_policy_modules=(./selinux/*.cil)
 
-policy_modules=(trivalent flatpakfull nautilus systemsettings thunar)
-
-cil_policy_modules=(
-    './selinux/user_namespace/grant_fm_userns.cil'
-    './selinux/user_namespace/grant_userns.cil'
-    './selinux/user_namespace/harden_userns.cil'
-    './selinux/user_namespace/harden_container_userns.cil'
-    './selinux/flatpakfull/grant_systemd_flatpak_exec.cil'
-    './selinux/user_namespace/userns_deny_unconfined_relabels.cil'
-)
-
-for module in "${policy_modules[@]}"; do
-    cd "./selinux/${module}"
-    make -f /usr/share/selinux/devel/Makefile "${module}.pp"
-    cd ../..
-done
-
-semodule -v -i ./selinux/*/*.pp "${cil_policy_modules[@]}"
+semodule -v -i "${cil_policy_modules[@]}"
 
 restorecon -FRv /usr

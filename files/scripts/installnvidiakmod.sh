77f0e680fa15a9fb52629d56c380f716950c263a
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-12-12 14:36:06 -0800

    fix: pin nvidia open version to 580 to prevent 590 beta regressions

--- files/scripts/installnvidiakmod.sh
+++ files/scripts/installnvidiakmod.sh
@@ -18,6 +18,7 @@ set -oue pipefail
 mkdir -p /var/tmp
 chmod 1777 /var/tmp
 
+PINNED_OPEN_VERSION="580.105.08-1"
 KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora.%_arch')"
 if [[ "$IMAGE_NAME" == *"open"* ]]; then
@@ -35,7 +36,11 @@ dnf install -y --setopt=install_weak_deps=False akmods gcc-c++
 cp /usr/sbin/akmodsbuild /usr/sbin/akmodsbuild.backup
 # TODO remove this when fixed upstream
 sed -i '/if \[\[ -w \/var \]\] ; then/,/fi/d' /usr/sbin/akmodsbuild
-dnf install -y --setopt=install_weak_deps=False nvidia-kmod-common nvidia-modprobe
+if [[ "$IMAGE_NAME" == *"open"* ]]; then
+    dnf install -y --setopt=install_weak_deps=False "nvidia-kmod-common-${PINNED_OPEN_VERSION}" "nvidia-modprobe-${PINNED_OPEN_VERSION}"
+else
+    dnf install -y --setopt=install_weak_deps=False nvidia-kmod-common nvidia-modprobe
+fi
 mv /usr/sbin/akmodsbuild.backup /usr/sbin/akmodsbuild
 
 echo "Installing kmod..."

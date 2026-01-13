d9dad6020533f634e1d573cda6f937fe89d5a8e4
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-01-13 13:39:30 -0800

    fix

--- files/scripts/installzfskmod.sh
+++ files/scripts/installzfskmod.sh
@@ -35,8 +35,8 @@ curl -fLsS --retry 5 \
 
 echo "Import key"
 # https://openzfs.github.io/openzfs-docs/Project%20and%20Community/Signing%20Keys.html
-gpg --yes --verbose --keyserver pgp.mit.edu --recv D4598027
-gpg --yes --verbose --keyserver pgp.mit.edu --recv C6AF658B
+gpg --yes --verbose --keyserver https://keyserver.ubuntu.com --recv D4598027
+gpg --yes --verbose --keyserver https://keyserver.ubuntu.com --recv C6AF658B
 
 echo "Verifying tar.gz signature"
 if ! gpg --verify "zfs-${ZFS_VERSION}.tar.gz.asc" "zfs-${ZFS_VERSION}.tar.gz"

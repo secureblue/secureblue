f6d0db5801f0a90c1753b3c1e02af3d71c24a65b
Qutiess <202758080+Qutiess@users.noreply.github.com>
2026-01-18 08:25:53 +0800

    Turned main part into a function & fixed an if statement

--- files/system/usr/libexec/secureblue/toggle_user_motd.py
+++ files/system/usr/libexec/secureblue/toggle_user_motd.py
@@ -12,17 +12,18 @@ from pathlib import Path
 import os
 
 # Extra parentheses added so python doesn't check the individual string instead of the path
-try:
-    os.remove(Path.home() / ".config" / "no-show-user-motd")
-    print("MOTD enabled.")
+def main():
+    try:
+        os.remove(Path.home() / ".config" / "no-show-user-motd")
+        print("MOTD enabled.")
 
-except FileNotFoundError:
-    if (Path.home() / ".config").is_dir() != True:
-        os.mkdir(Path.home() / ".config")
-    (Path.home() / ".config" / "no-show-user-motd").touch(exist_ok=False)
-    print("MOTD disabled.")
+    except FileNotFoundError:
+        if (Path.home() / ".config").is_dir() != True:
+            os.mkdir(Path.home() / ".config")
+        (Path.home() / ".config" / "no-show-user-motd").touch(exist_ok=False)
+        print("MOTD disabled.")
 
 
 
 if __name__ == "__main__":
-     sys.exit(main())
+     main()

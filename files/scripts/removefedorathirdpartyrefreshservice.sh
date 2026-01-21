97ebb11038efc6151b63866f455fa3f246d44a8b
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2026-01-20 23:34:17 -0300

    feat: add build script to remove fedora third-party refresh service

---
+++ files/scripts/removefedorathirdpartyrefreshservice.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+rm -f /usr/lib/systemd/system/fedora-third-party-refresh.service

0f2341b9be8678fe9d05ebf8ade9e9c333140cf2
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2026-01-20 23:57:53 -0300

    style: fix copyright date

--- files/scripts/removefedorathirdpartyrefreshservice.sh
+++ files/scripts/removefedorathirdpartyrefreshservice.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+# SPDX-FileCopyrightText: Copyright 2026 The Secureblue Authors
 #
 # SPDX-License-Identifier: Apache-2.0
 

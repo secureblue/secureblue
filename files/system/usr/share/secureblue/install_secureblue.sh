bc88fc717ba490c3a824ff0e79bd3565e7f4942e
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2026-01-22 20:26:00 -0300

    feat: add warning if missing deps for provenance

--- files/system/usr/share/secureblue/install_secureblue.sh
+++ files/system/usr/share/secureblue/install_secureblue.sh
@@ -101,16 +101,60 @@ fi
 printf "Command to execute:\n%s\n\n" "$rebase_command"
 
 read -rp "Proceed? (yes/No): " rebase_proceed
-if is_yes "$rebase_proceed"; then
-    eval "$rebase_command"
-fi
+echo
 
 # Check if we should verify provenance
-if [ "${REBASE_SECUREBLUE_PROVENANCE}" == "true" ]; then
+PROVENANCE_ON_REBASE=true
+PROVENANCE_NO_CRANE=false
+PROVENANCE_NO_SLSA_VERIFIER=false
+
+if ! command -v crane &> /dev/null || ! command -v slsa-verifier &> /dev/null
+then
+    PROVENANCE_ON_REBASE=false
+
+    if ! command -v crane &> /dev/null
+    then
+        PROVENANCE_NO_CRANE=true
+    fi
+
+    if ! command -v slsa-verifier &> /dev/null
+    then
+        PROVENANCE_NO_SLSA_VERIFIER=true
+    fi
+fi
+
+if [[ "${PROVENANCE_NO_CRANE}" == "true" && "${PROVENANCE_NO_SLSA_VERIFIER}" == "true" ]]
+then
+    echo -e "⚠️\033[31m WARNING: COMMANDS 'crane' and 'slsa-verifier' ARE NOT AVAILABLE ON YOUR SYSTEM.\nWITHOUT THEM, YOU WILL NOT BE ABLE TO VERIFY THE PROVENANCE OF IMAGES."
+    echo -e "\033[0m"
+    read -rp "Proceed anyway? [NOT RECOMMENDED] (yes/No): " provenance_faulty_proceed
     echo
+elif [[ "${PROVENANCE_NO_CRANE}" == "false" && "${PROVENANCE_NO_SLSA_VERIFIER}" == "true" ]]
+then
+    echo -e "⚠️\033[31m WARNING: COMMAND 'crane' IS NOT AVAILABLE ON YOUR SYSTEM.\nWITHOUT IT, YOU WILL NOT BE ABLE TO VERIFY THE PROVENANCE OF IMAGES."
+    echo -e "\033[0m"
+    read -rp "Proceed anyway? [NOT RECOMMENDED] (yes/No): " provenance_faulty_proceed
+    echo
+elif [[ "${PROVENANCE_NO_CRANE}" == "true" && "${PROVENANCE_NO_SLSA_VERIFIER}" == "false" ]]
+then
+    echo -e "⚠️\033[31m WARNING: COMMAND 'slsa-verifier' IS NOT AVAILABLE ON YOUR SYSTEM.\nWITHOUT IT, YOU WILL NOT BE ABLE TO VERIFY THE PROVENANCE OF IMAGES."
+    echo -e "\033[0m"
+    read -rp "Proceed anyway? [NOT RECOMMENDED] (yes/No): " provenance_faulty_proceed
+    echo
+fi
+
+if is_yes "$provenance_faulty_proceed"; then
+    /usr/bin/true
+fi
+
+if [[ "${PROVENANCE_ON_REBASE}" == "true" ]]; then
     echo Verifying image provenance...
     echo
     rebase_crane=$(crane digest --full-ref "ghcr.io/secureblue/${image_name}:latest")
-    export rebase_crane
     slsa-verifier verify-image --source-uri "github.com/secureblue/secureblue" --source-branch "live" "${rebase_crane}"
+    echo
+fi
+
+if is_yes "$rebase_proceed"; then
+    eval "$rebase_command"
 fi

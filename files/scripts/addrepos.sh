df8999e9f0539891620f288ca399cea430e95263
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-01-26 14:22:40 -0800

    comment

--- files/scripts/addrepos.sh
+++ files/scripts/addrepos.sh
@@ -7,16 +7,18 @@
 
 set -euo pipefail
 
+readonly OS_VERSION=43
+
 # We never use this repo, so remove it early to prevent failed calls to it
 rm -f /etc/yum.repos.d/fedora-cisco-openh264.repo
 
 install_repo() {
-  versioned_repo="${1//%OS_VERSION%/43}"
-  curl -fLsS --retry 5 -o "/etc/yum.repos.d/${versioned_repo##*/}" "$versioned_repo"
+  repo="${1}"
+  curl -fLsS --retry 5 -o "/etc/yum.repos.d/${repo}" "${repo}"
 }
 
 common_repos=(
-  "https://copr.fedorainfracloud.org/coprs/secureblue/packages/repo/fedora-%OS_VERSION%/secureblue-packages-fedora-%OS_VERSION%.repo"
+  "https://copr.fedorainfracloud.org/coprs/secureblue/packages/repo/fedora-$OS_VERSION/secureblue-packages-fedora-$OS_VERSION.repo"
   "https://negativo17.org/repos/fedora-multimedia.repo"
 )
 

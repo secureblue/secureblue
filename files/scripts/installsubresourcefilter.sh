0c8605afd78d582019d4d19e6298593006682367
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-01-25 17:40:59 -0800

    fix

---
+++ files/scripts/installsubresourcefilter.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+echo '
+[copr:copr.fedorainfracloud.org:secureblue:packages]
+name=Copr repo for trivalent owned by secureblue
+baseurl=https://download.copr.fedorainfracloud.org/results/secureblue/trivalent/fedora-$releasever-x86_64/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://download.copr.fedorainfracloud.org/results/secureblue/trivalent/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1
+priority=99
+' > /etc/yum.repos.d/secureblue-packages-x86_64-fedora-43.repo
+
+dnf install -y --setopt=install_weak_deps=False trivalent-subresource-filter
+
+rm -f /etc/yum.repos.d/secureblue-packages-x86_64-fedora-43.repo

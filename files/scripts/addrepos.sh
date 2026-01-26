ef2748c21b70b406031b6f9d2a8488bcb32e260c
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-01-25 17:38:09 -0800

    chore: migrate to combined copr

--- files/scripts/addrepos.sh
+++ files/scripts/addrepos.sh
@@ -16,18 +16,8 @@ install_repo() {
 }
 
 common_repos=(
-  "https://copr.fedorainfracloud.org/coprs/secureblue/crane/repo/fedora-%OS_VERSION%/secureblue-crane-fedora-%OS_VERSION%.repo"
-  "https://copr.fedorainfracloud.org/coprs/secureblue/slsa-verifier/repo/fedora-%OS_VERSION%/secureblue-slsa-verifier-fedora-%OS_VERSION%.repo"
-  "https://copr.fedorainfracloud.org/coprs/secureblue/no_rlimit_as/repo/fedora-%OS_VERSION%/secureblue-no_rlimit_as-fedora-%OS_VERSION%.repo"
-  "https://copr.fedorainfracloud.org/coprs/secureblue/hardened_malloc/repo/fedora-%OS_VERSION%/secureblue-hardened_malloc-fedora-%OS_VERSION%.repo"
-  "https://copr.fedorainfracloud.org/coprs/secureblue/run0edit/repo/fedora-%OS_VERSION%/secureblue-run0edit-fedora-%OS_VERSION%.repo"
+  "https://copr.fedorainfracloud.org/coprs/secureblue/packages/repo/fedora-%OS_VERSION%/secureblue-packages-fedora-%OS_VERSION%.repo"
   "https://negativo17.org/repos/fedora-multimedia.repo"
-)
-
-desktop_repos=(
-  "https://copr.fedorainfracloud.org/coprs/secureblue/bubblejail/repo/fedora-%OS_VERSION%/secureblue-bubblejail-fedora-%OS_VERSION%.repo"
-  "https://copr.fedorainfracloud.org/coprs/secureblue/branding/repo/fedora-%OS_VERSION%/secureblue-branding-fedora-%OS_VERSION%.repo"
-  "https://copr.fedorainfracloud.org/coprs/secureblue/homebrew/repo/fedora-%OS_VERSION%/secureblue-homebrew-fedora-%OS_VERSION%.repo"
 )
 
 server_repos=(
@@ -40,10 +30,6 @@ done
 
 if [[ "$IMAGE_NAME" == *"iot"* || "$IMAGE_NAME" == *"securecore"* ]]; then
   for repo in "${server_repos[@]}"; do
-    install_repo "$repo"
-  done
-else
-  for repo in "${desktop_repos[@]}"; do
     install_repo "$repo"
   done
 fi

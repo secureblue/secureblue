8d7bbed4039654e4dc4704ce98119f49fe23cd97
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-01-25 21:18:23 -0800

    Update installsubresourcefilter.sh

--- files/scripts/installsubresourcefilter.sh
+++ files/scripts/installsubresourcefilter.sh
@@ -4,17 +4,18 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+set -oue pipefail
 
 # This is a noarch package that we only build on x86_64, for both arches. As such, this is 
 # hardcoded to x86_64 deliberately.
 echo '
 [copr:copr.fedorainfracloud.org:secureblue:packages]
 name=Copr repo for trivalent owned by secureblue
-baseurl=https://download.copr.fedorainfracloud.org/results/secureblue/trivalent/fedora-$releasever-x86_64/
+baseurl=https://download.copr.fedorainfracloud.org/results/secureblue/packages/fedora-$releasever-x86_64/
 type=rpm-md
 skip_if_unavailable=True
 gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/secureblue/trivalent/pubkey.gpg
+gpgkey=https://download.copr.fedorainfracloud.org/results/secureblue/packages/pubkey.gpg
 repo_gpgcheck=0
 enabled=1
 enabled_metadata=1

c9d6e2868e48dd1bbf7f407b355a3f4e35fba4e2
spaceoden <26421479+spaceoden@users.noreply.github.com>
2026-01-26 18:28:58 -0800

    Update install_secureblue.sh to prevent server/desktop rebase

--- files/system/usr/share/secureblue/install_secureblue.sh
+++ files/system/usr/share/secureblue/install_secureblue.sh
@@ -53,12 +53,24 @@ if is_yes "$is_server"; then
         echo "Refusing to proceed."
         exit 1
     fi
+    if grep -E 'VARIANT_ID=silverblue|VARIANT_ID=kinoite|VARIANT_ID=sericea|VARIANT_ID=cosmic' /etc/os-release >/dev/null; then
+        echo "The current operating system is a desktop."
+        echo "rebasing between server/desktop can cause critical issues (like not booting)."
+        echo "Refusing to proceed."
+        exit 1
+    fi
     read -rp "Do you need ZFS support? (yes/No): " use_zfs
     image_name=$(is_yes "$use_zfs" && echo "securecore-zfs" || echo "securecore")
 else
     if grep VARIANT=\"CoreOS\" /etc/os-release >/dev/null; then
         echo "The current operating system is based on CoreOS."
         echo "Fedora Atomic and CoreOS use different partitioning schemes and are not compatible."
+        echo "Refusing to proceed."
+        exit 1
+    fi
+    if grep -E 'VARIANT_ID=iot|VARIANT_ID=securecore' /etc/os-release >/dev/null; then
+        echo "The current operating system is a server."
+        echo "rebasing between server/desktop can cause critical issues (like not booting)."
         echo "Refusing to proceed."
         exit 1
     fi

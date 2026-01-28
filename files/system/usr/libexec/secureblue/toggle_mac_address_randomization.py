03b4993a8f57ac5656acc320b3edb7135a89f975
M <meijuta@proton.me>
2026-01-28 22:54:53 +1100

    Handling for disabling and start on user interaction stuff

--- files/system/usr/libexec/secureblue/toggle_mac_address_randomization.py
+++ files/system/usr/libexec/secureblue/toggle_mac_address_randomization.py
@@ -8,13 +8,42 @@
 Toggles MAC address randomisation
 """
 
-'''
-TODO:
-- Prepare tests
--
-'''
-
 import os
 import sys
 from pathlib import Path
+
+RAND_MAC_FILE="/etc/NetworkManager/conf.d/rand_mac.conf"
+
+if RAND_MAC_FILE.exists():
+
+    os.remove(RAND_MAC_FILE)
+    print("MAC randomization disabled")
+
+    # bounce the connection
+
+    connection_profile_name = subprocess.run(["nmcli", "-t", "-f", "NAME,DEVICE", "connection", "show", "--active"]).stdout.strip()
+    # check if there is wifi
+    if connection_profile_name == "lo:lo":
+        pass
+    else:
+        connection_profile_name = connection_profile_name.split(':')[0] # The output of the above contains excess data, which can be removed by only taking the part of the string before the first ':'
+        subprocess.run(["nmcli", "connection", "down", connection_profile_name])
+        subprocess.run(["nmcli", "connection", "up", connection_profile_name])
+
+else:
+
+    print("MAC randomization can be stable (persisting the same random MAC per access point across disconnects/reboots),")
+    print("or it can be randomized per-connection (every time it connects to the same access point it uses a new MAC).")
+
+    randomization_choice = input("Do you want to use per-connection Wi-Fi MAC address randomization? [y/N] ")
+
+    if randomization_choice in "Yy":
+        randomization_level = 'random'
+        print("Selected state: per-connection")
+    else:
+        randomization_level = "stable"
+        print("Selected state: per-network (stable)")
+
+
+
 

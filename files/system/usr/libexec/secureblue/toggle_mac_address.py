4fa8e7130a82afd123bf069b8cf127e4d3544b7d
M <meijuta@proton.me>
2026-01-28 21:25:25 +1100

    testing gpg signing key

---
+++ files/system/usr/libexec/secureblue/toggle_mac_address.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+The wheel user creation implementation for ujust
+"""
+
+'''
+TODO:
+- Prepare tests
+-
+'''
+
+import os
+import sys
+from pathlib import Path
+

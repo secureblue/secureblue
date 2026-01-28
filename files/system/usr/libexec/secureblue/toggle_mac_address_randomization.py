a0a20ddf186e45f3d34f739a657368348a695431
M <meijuta@proton.me>
2026-01-29 00:51:23 +1100

    Applied ruff formatting.

--- files/system/usr/libexec/secureblue/toggle_mac_address_randomization.py
+++ files/system/usr/libexec/secureblue/toggle_mac_address_randomization.py
@@ -12,37 +12,51 @@ import os
 import subprocess
 from pathlib import Path
 
-RAND_MAC_FILE="/etc/NetworkManager/conf.d/rand_mac.conf"
+RAND_MAC_FILE = "/etc/NetworkManager/conf.d/rand_mac.conf"
 
 if Path(RAND_MAC_FILE).exists():
-
     os.remove(RAND_MAC_FILE)
     print("MAC randomization disabled")
 
     # bounce the connection to refresh MAC address with host
 
-    active_connections = subprocess.run(["nmcli", "-t", "-f", "NAME,DEVICE,TYPE", "connection", "show", "--active"], capture_output = True, text = True).stdout.strip().splitlines()
+    active_connections = (
+        subprocess.run(
+            ["nmcli", "-t", "-f", "NAME,DEVICE,TYPE", "connection", "show", "--active"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        .stdout.strip()
+        .splitlines()
+    )
     # check if there is wifi
     # handle multiple connections
     for connection in active_connections:
-
         try:
-            name, device, con_type = connection.split(':', 2)
+            name, device, con_type = connection.split(":", 2)
         except ValueError:
-            continue # skipping malformed lines
+            continue  # skipping malformed lines
 
-        if con_type != "wifi": continue # dont disconnect from ethernet needlessly
-        if device == "lo": continue
+        if con_type != "wifi":
+            continue  # dont disconnect from ethernet needlessly
+        if device == "lo":
+            continue
 
-        subprocess.run(["nmcli", "connection", "down", name], check = True)
-        subprocess.run(["nmcli", "connection", "up", name], check = True)
+        subprocess.run(["nmcli", "connection", "down", name], check=True)
+        subprocess.run(["nmcli", "connection", "up", name], check=True)
 
 else:
+    print(
+        "MAC randomization can be stable (persisting the same random MAC per access point across disconnects/reboots),"
+    )
+    print(
+        "or it can be randomized per-connection (every time it connects to the same access point it uses a new MAC)."
+    )
 
-    print("MAC randomization can be stable (persisting the same random MAC per access point across disconnects/reboots),")
-    print("or it can be randomized per-connection (every time it connects to the same access point it uses a new MAC).")
-
-    randomization_choice = input("Do you want to use per-connection Wi-Fi MAC address randomization? [y/N] ")
+    randomization_choice = input(
+        "Do you want to use per-connection Wi-Fi MAC address randomization? [y/N] "
+    )
 
     if randomization_choice in "Yy":
         randomization_level = "random"
@@ -51,7 +65,7 @@ else:
         randomization_level = "stable"
         print("Selected state: per-network (stable)")
 
-    with open(RAND_MAC_FILE, 'w') as f:
+    with open(RAND_MAC_FILE, "w") as f:
         f.write(
             "[device-mac-randomization]\n"
             # "yes" is already the default for scanning
@@ -59,26 +73,36 @@ else:
             "[connection-mac-randomization]\n"
             # Generate a random MAC for each Network and associate the two permanently.
             "ethernet.cloned-mac-address=stable\n"
-            "wifi.cloned-mac-address=" + randomization_level + '\n'
-            )
+            "wifi.cloned-mac-address=" + randomization_level + "\n"
+        )
 
     os.chmod(RAND_MAC_FILE, 0o644)
     print("MAC randomization enabled.")
 
     # bounce the connection to refresh MAC address with host
 
-    active_connections = subprocess.run(["nmcli", "-t", "-f", "NAME,DEVICE,TYPE", "connection", "show", "--active"], capture_output = True, text = True).stdout.strip().splitlines()
+    active_connections = (
+        subprocess.run(
+            ["nmcli", "-t", "-f", "NAME,DEVICE,TYPE", "connection", "show", "--active"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        .stdout.strip()
+        .splitlines()
+    )
     # check if there is wifi
     # handle multiple connections
     for connection in active_connections:
-
         try:
-            name, device, con_type = connection.split(':', 2)
+            name, device, con_type = connection.split(":", 2)
         except ValueError:
-            continue # skipping malformed lines
+            continue  # skipping malformed lines
 
-        if con_type != "wifi": continue # dont disconnect from ethernet needlessly
-        if device == "lo": continue
+        if con_type != "wifi":
+            continue  # dont disconnect from ethernet needlessly
+        if device == "lo":
+            continue
 
-        subprocess.run(["nmcli", "connection", "down", name], check = True)
-        subprocess.run(["nmcli", "connection", "up", name], check = True)
+        subprocess.run(["nmcli", "connection", "down", name], check=True)
+        subprocess.run(["nmcli", "connection", "up", name], check=True)

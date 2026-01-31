b504fb40290db86e1b9f54e53d03c3ce1ad9d0e5
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-01-31 14:49:52 -0800

    Update removesuid.sh

--- files/scripts/removesuid.sh
+++ files/scripts/removesuid.sh
@@ -83,13 +83,13 @@ set_caps_if_present "cap_sys_admin=ep" "/usr/bin/fusermount3"
 set_caps_if_present "cap_dac_read_search,cap_audit_write=ep" "/usr/sbin/unix_chkpwd"
 
 # spice-client-glib-usb-acl-helper drops all capabilities except CAP_FOWNER:
-# should be added back.
 # https://gitlab.freedesktop.org/spice/spice-gtk/-/blob/7a2779182b003ec5e8192dc5186f0b1c3eb8e831/src/spice-client-glib-usb-acl-helper.c#L304
 set_caps_if_present "cap_fowner=ep" "/usr/libexec/spice-gtk-$(uname -m)/spice-client-glib-usb-acl-helper"
 
 # The below capabilities are expected by these QEMU-related executables but do
 # not seem to be needed for ordinary libvirt/QEMU/KVM usage. They are left
 # commented out for reference in case we later determine that the capabilities
+# should be added back.
 # Mounting and unmounting requires CAP_SYS_ADMIN:
 # set_caps_if_present "cap_sys_admin=ep" "/usr/bin/fusermount-glusterfs"
 

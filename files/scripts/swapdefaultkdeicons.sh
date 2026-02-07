c75959998c846ee0ca03776fa10a10a08187e346
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-02-07 14:29:10 -0800

    Update swapdefaultkdeicons.sh

--- files/scripts/swapdefaultkdeicons.sh
+++ files/scripts/swapdefaultkdeicons.sh
@@ -8,4 +8,4 @@
 
 # sets default/pinned applications on the taskmanager applet on the panel, there is no nice way to do this
 # https://bugs.kde.org/show_bug.cgi?id=511560
-sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>applications:systemsettings.desktop,applications:io.github.kolunmi.Bazaar.desktop,preferred:\/\/filemanager,preferred:\/\/browser<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
+sed -i 's/\<applications:org\.kde\.discover\.desktop\>/applications:io.github.kolunmi.Bazaar.desktop/g' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml

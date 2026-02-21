6a2f8be57ce72cdcd1e1476f9012a49716e1b161
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-02-21 15:17:04 -0800

    fix(iot): multiple dns issues

---
+++ files/scripts/remove-dns-greenboot-check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+rm -f /usr/lib/greenboot/check/required.d/01_repository_dns_check.sh

73bc94eaa8363b3d54ef04e0eebf1d32117026fa
Daniel Hast <hast.daniel@protonmail.com>
2026-03-13 20:28:34 -0400

    feat: install Trivalent SELinux policy via RPM
    Trivalent's SELinux policy is now built as an RPM subpackage of
    Trivalent and can be installed alongside Trivalent using dnf. The source
    code of the SELinux policy has moved to the Trivalent repo.
    
    Also properly mark optional policy blocks in CIL so the userns policies
    work even when certain optional policy modules aren't present (for
    example, Trivalent's policy module on server images).

--- files/scripts/install-trivalent.sh
+++ files/scripts/install-trivalent.sh
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
+shopt -s nullglob
 
 ARCH="$(uname -m)"
 
@@ -14,31 +15,48 @@ curl -fLsS --retry 5 -o /etc/yum.repos.d/repo.secureblue.dev.secureblue.repo htt
 secureblue_gpg_key_path="$(dnf repo info secureblue --json | jq -r '.[0].gpg_key.[0]')"
 rpmkeys --import "${secureblue_gpg_key_path}"
 
-# The package signature is NOT being checked at this stage,
+# Package signatures are NOT being checked at this stage,
 # see https://github.com/rpm-software-management/dnf5/issues/1985
 dnf --best --repo=secureblue -y download trivalent
 
 trivalent_rpms_found=0
-for trivalent_rpm in trivalent-*."${ARCH}".rpm; do
+for trivalent_rpm in trivalent-[0-9]*."${ARCH}".rpm; do
     (( ++trivalent_rpms_found ))
 done
 
-if [ "$trivalent_rpms_found" -eq 1 ]; then
-    echo "Found: ${trivalent_rpms_found}"
+if (( trivalent_rpms_found == 1 )); then
+    echo "Trivalent RPM: ${trivalent_rpm}"
 else
-    echo "Number of trivalent rpms not one, found: ${trivalent_rpms_found}"
+    echo "Number of Trivalent RPMs not one, found: ${trivalent_rpms_found}"
     exit 1
 fi
 
 trivalent_rpm_sans_prefix=${trivalent_rpm#trivalent-}
 trivalent_version=${trivalent_rpm_sans_prefix%".${ARCH}.rpm"}
 
-provenance_file="${trivalent_rpm}.intoto.jsonl"
-wget "https://github.com/secureblue/Trivalent/releases/download/${trivalent_version}/${provenance_file}"
+trivalent_selinux_pkg="trivalent-selinux-${trivalent_version}"
+dnf --repo=secureblue -y download "${trivalent_selinux_pkg}"
 
-slsa-verifier verify-artifact "${trivalent_rpm}" --provenance-path "${provenance_file}" --source-uri github.com/secureblue/Trivalent --source-branch live
+trivalent_selinux_rpm="${trivalent_selinux_pkg}.noarch.rpm"
 
-# Forcing GPG check for a package installed outside of a repository
-dnf --setopt=localpkg_gpgcheck=True --setopt=install_weak_deps=False -y install "${trivalent_rpm}"
+if [[ -f "${trivalent_selinux_rpm}" ]]; then
+    echo "Trivalent SELinux policy RPM: ${trivalent_selinux_rpm}"
+else
+    echo "trivalent-selinux RPM not found"
+    exit 1
+fi
+
+provenance_file="multiple.intoto.jsonl"
+curl -fLsS --retry 5 -O "https://github.com/secureblue/Trivalent/releases/download/${trivalent_version}/${provenance_file}"
+
+slsa-verifier verify-artifact \
+    --provenance-path "${provenance_file}" \
+    --source-uri 'github.com/secureblue/Trivalent' \
+    --source-branch 'live' \
+    "${trivalent_rpm}" "${trivalent_selinux_rpm}"
+
+# Forcing GPG check for packages installed outside of a repository
+dnf --setopt=localpkg_gpgcheck=True --setopt=install_weak_deps=False -y \
+    install "${trivalent_rpm}" "${trivalent_selinux_rpm}"
 
 sed -i 's/org\.mozilla\.firefox\.desktop/trivalent.desktop/' /usr/share/applications/mimeapps.list

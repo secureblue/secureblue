2021ed91cee439ff7316a116b180274952b1f588
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-03-19 18:50:44 -0700

    various fixes

--- .github/workflows/isos/allow_squashfs.sh
+++ .github/workflows/isos/allow_squashfs.sh
@@ -9,7 +9,15 @@ set -euo pipefail
 sed -i '/^install squashfs \/bin\/false$/d' /usr/lib/modprobe.d/secureblue.conf
 
 systemctl disable bootloader-update.service
-dnf remove -y google-noto-fonts-all
+systemctl disable rpm-ostree-countme.service
+
+dnf remove -y google-noto-fonts-all homebrew
 dnf install -y fedora-logos secureblue-logos
 dnf install -y anaconda-live libblockdev-btrfs
 dnf reinstall -y polkit 
+
+mkdir -p /etc/skel/.local/share/keyrings
+gnome-keyring-daemon --start --components=secrets &
+echo "" | secret-tool store --label=init init init 2>/dev/null || true
+cp ~/.local/share/keyrings/login.keyring /etc/skel/.local/share/keyrings/login.keyring
+chmod 600 /etc/skel/.local/share/keyrings/login.keyring

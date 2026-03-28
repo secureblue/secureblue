0f6465f5fdd57b141aa9dbb47a669a58705e8562
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-03-27 18:56:43 -0700

    fix build failure

--- .github/workflows/isos/prep_rootfs.sh
+++ .github/workflows/isos/prep_rootfs.sh
@@ -157,7 +157,6 @@ bootc switch --mutate-in-place --enforce-container-sigpolicy --transport registr
 %end
 EOF
 
-mkdir -p /usr/local/bin
 tee /usr/local/bin/trivalent-webui <<EOF
 #!/bin/bash
 exec /usr/bin/trivalent --app="$1" --user-data-dir=/tmp/anaconda-trivalent-profile  --disable-infobars --no-first-run --kiosk "$@"

074dc72666ba98831048059819f56e0d1fcad21e
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-04-09 21:55:32 -0700

    chore: remove suid from polkit and secureblue libraries

--- files/scripts/removesuid.sh
+++ files/scripts/removesuid.sh
@@ -11,26 +11,6 @@ set -euo pipefail
 whitelist=(
     # Required for nvidia closed driver images
     "/usr/bin/nvidia-modprobe"
-    # https://gitlab.freedesktop.org/polkit/polkit/-/issues/168
-    "/usr/lib/polkit-1/polkit-agent-helper-1"
-    # https://github.com/secureblue/secureblue/issues/119
-    # Required for hardened_malloc to be used by suid-root processes
-    "/usr/lib64/libhardened_malloc-light.so"
-    "/usr/lib64/libhardened_malloc-pkey.so"
-    "/usr/lib64/libhardened_malloc.so"
-    "/usr/lib64/glibc-hwcaps/x86-64/libhardened_malloc-light.so"
-    "/usr/lib64/glibc-hwcaps/x86-64/libhardened_malloc-pkey.so"
-    "/usr/lib64/glibc-hwcaps/x86-64/libhardened_malloc.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v2/libhardened_malloc-light.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v2/libhardened_malloc-pkey.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v2/libhardened_malloc.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v3/libhardened_malloc-light.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v3/libhardened_malloc-pkey.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v3/libhardened_malloc.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v4/libhardened_malloc-light.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v4/libhardened_malloc-pkey.so"
-    "/usr/lib64/glibc-hwcaps/x86-64-v4/libhardened_malloc.so"
-    "/usr/lib64/libno_rlimit_as.so"
 )
 
 

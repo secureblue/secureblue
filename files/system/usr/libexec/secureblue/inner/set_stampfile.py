61fc8c432a33185bd7aec4f896a00655a950a543
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-04-23 12:21:05 -0700

    feat: add ujust set-always-upgrade-on-boot

---
+++ files/system/usr/libexec/secureblue/inner/set_stampfile.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Create or delete a stampfile
+"""
+
+import enum
+import sys
+
+
+class Mode(enum.StrEnum):
+    """Enum representing 'create' or 'delete'."""
+
+    CREATE = "create"
+    DELETE = "delete"
+
+
+def set_stampfile(mode: Mode, stampfile_path: str) -> int:
+    """Enable or disable an SELinux module."""
+    match mode:
+        case Mode.CREATE:
+            stampfile = Path(stampfile_path)
+            stampfile.parent.mkdir(parents=True, exist_ok=True)
+            stampfile.touch()
+            Path(stampfile_path).touch()
+        case Mode.DELETE:
+            Path(stampfile_path).unlink(missing_ok=True)
+        case _:
+            print(f"No such mode: {mode}")
+            return 1
+    return 0
+
+
+def main() -> int:
+    """Main script entry point."""
+    required_args_count = 3
+    if len(sys.argv) != required_args_count:
+        print("set_stampfile.py must have exactly two arguments.")
+        return 2
+
+    try:
+        mode = Mode(sys.argv[1])
+    except ValueError:
+        print("Invalid argument: first argument must be 'create' or 'delete'.")
+        return 2
+
+    stampfile_path = sys.argv[2]
+    return set_stampfile(mode, stampfile_path)
+
+
+if __name__ == "__main__":
+    sys.exit(main())

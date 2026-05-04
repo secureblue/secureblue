c6f99222842b01e2bba31574183e4d4a961f373f
Daniel Hast <hast.daniel@protonmail.com>
2026-05-04 15:28:56 -0400

    feat: move public keys from /etc/pki to /usr/share/pki
    * Move container image public keys from `/etc/pki/containers` to
      `/usr/share/pki/containers`, and modify `/etc/containers/policy.json`
      and various scripts to match. Also make symlinks from the old key
      locations to the new key locations to avoid breaking image signature
      verification for users who don't receive an updated policy.json due to
      local modifications.
    * Move akmods public keys from `/etc/pki/akmods` to
      `/usr/share/pki/akmods`, and modify scripts to match.
    * Minor code simplifications and style improvements in `signkernel.sh`
      and `signmodules.sh` (since those were being modified anyway).
    
    Assisted-by: GNU sed 4.9

--- files/scripts/signmodules.sh
+++ files/scripts/signmodules.sh
@@ -8,40 +8,47 @@
 set -euo pipefail
 
 MODULE_NAME="${1-}"
-if [ -z "$MODULE_NAME" ]; then
-  echo "MODULE_NAME is empty. Exiting..."
+if [[ -z "${MODULE_NAME}" ]]; then
+  echo 'MODULE_NAME is empty. Exiting...'
   exit 1
 fi
 
-KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KERNEL_VERSION="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-PUBLIC_KEY_DER_PATH="../system/etc/pki/akmods/certs/akmods-secureblue.der"
-PUBLIC_KEY_CRT_PATH="./certs/public_key.crt"
-PRIVATE_KEY_PATH="/tmp/certs/private_key.priv"
-openssl x509 -in "$PUBLIC_KEY_DER_PATH" -out "$PUBLIC_KEY_CRT_PATH"
+PUBLIC_KEY_DER_PATH='../system/usr/share/pki/akmods/certs/akmods-secureblue.der'
+PUBLIC_KEY_CRT_PATH='./certs/public_key.crt'
+PRIVATE_KEY_PATH='/tmp/certs/private_key.priv'
+openssl x509 -in "${PUBLIC_KEY_DER_PATH}" -out "${PUBLIC_KEY_CRT_PATH}"
 
-PRIVATE_KEY_PATH="/tmp/certs/private_key.priv"
-SIGNING_KEY="./certs/signing_key.pem"
-cat "$PRIVATE_KEY_PATH" <(echo) "$PUBLIC_KEY_CRT_PATH" >> "$SIGNING_KEY"
+PRIVATE_KEY_PATH='/tmp/certs/private_key.priv'
+SIGNING_KEY='./certs/signing_key.pem'
+cat "${PRIVATE_KEY_PATH}" <(echo) "${PUBLIC_KEY_CRT_PATH}" >> "${SIGNING_KEY}"
 
-for module in /usr/lib/modules/"${KERNEL_VERSION}"/extra/"${MODULE_NAME}"/*.ko*; do
-    module_basename="${module:0:-3}"
-    module_suffix="${module: -3}"
-    if [[ "$module_suffix" == ".xz" ]]; then
-        xz --decompress "$module"
-        openssl cms -sign -signer "${SIGNING_KEY}" -binary -in "$module_basename" -outform DER -out "${module_basename}.cms" -nocerts -noattr -nosmimecap
-        /usr/src/kernels/"${KERNEL_VERSION}"/scripts/sign-file -s "${module_basename}.cms" sha256 "${PUBLIC_KEY_CRT_PATH}" "${module_basename}"
-        /bin/bash ./sign-check.sh "${KERNEL_VERSION}" "${module_basename}" "${PUBLIC_KEY_CRT_PATH}"
+sign_module() {
+    local module="$1"
+    openssl cms -sign -signer "${SIGNING_KEY}" -binary -in "${module}" \
+        -outform DER -out "${module}.cms" -nocerts -noattr -nosmimecap
+    "/usr/src/kernels/${KERNEL_VERSION}/scripts/sign-file" -s "${module}.cms" \
+        sha256 "${PUBLIC_KEY_CRT_PATH}" "${module}"
+    /bin/bash ./sign-check.sh "${KERNEL_VERSION}" "${module}" "${PUBLIC_KEY_CRT_PATH}"
+}
+
+for module in "/usr/lib/modules/${KERNEL_VERSION}/extra/${MODULE_NAME}"/*.ko*; do
+    module_basename="${module%.*}"
+    module_suffix=".${module##*.}"
+    case "${module_suffix}" in
+    .xz)
+        xz --decompress "${module}"
+        sign_module "${module_basename}"
         xz -C crc32 -f "${module_basename}"
-    elif [[ "$module_suffix" == ".gz" ]]; then
-        gzip -d "$module"
-        openssl cms -sign -signer "${SIGNING_KEY}" -binary -in "$module_basename" -outform DER -out "${module_basename}.cms" -nocerts -noattr -nosmimecap
-        /usr/src/kernels/"${KERNEL_VERSION}"/scripts/sign-file -s "${module_basename}.cms" sha256 "${PUBLIC_KEY_CRT_PATH}" "${module_basename}"
-        /bin/bash ./sign-check.sh "${KERNEL_VERSION}" "${module_basename}" "${PUBLIC_KEY_CRT_PATH}"
+        ;;
+    .gz)
+        gzip -d "${module}"
+        sign_module "${module_basename}"
         gzip -9f "${module_basename}"
-    else
-        openssl cms -sign -signer "${SIGNING_KEY}" -binary -in "$module" -outform DER -out "${module}.cms" -nocerts -noattr -nosmimecap
-        /usr/src/kernels/"${KERNEL_VERSION}"/scripts/sign-file -s "${module}.cms" sha256 "${PUBLIC_KEY_CRT_PATH}" "${module}"
-        /bin/bash ./sign-check.sh "${KERNEL_VERSION}" "${module}" "${PUBLIC_KEY_CRT_PATH}"
-    fi
+        ;;
+    *)
+        sign_module "${module}"
+        ;;
+    esac
 done

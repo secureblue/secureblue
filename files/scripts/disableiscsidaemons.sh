ac4a37fd827a03fcd81d1e570fc405ae0bdbccbd
dentist8204 <241748175+dentist8204@users.noreply.github.com>
2026-05-23 21:22:53 -0500

    Update disableiscsidaemons.sh
    Fix copy-paste typo.

--- files/scripts/disableiscsidaemons.sh
+++ files/scripts/disableiscsidaemons.sh
@@ -17,5 +17,5 @@ systemctl mask iscsid.socket 2>/dev/null || true
 systemctl disable iscsiuio.service 2>/dev/null || true
 systemctl mask iscsiuio.service 2>/dev/null || true
 
-systemctl disable iscsid.socket 2>/dev/null || true
-systemctl mask iscsid.socket 2>/dev/null || true
+systemctl disable iscsiuio.socket 2>/dev/null || true
+systemctl mask iscsiuio.socket 2>/dev/null || true

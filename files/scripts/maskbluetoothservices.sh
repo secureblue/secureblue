4d71fba8cf20ccd67ee24aa0257b99758d1f052e
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-06-04 15:46:20 -0700

    remove obex

--- files/scripts/maskbluetoothservices.sh
+++ files/scripts/maskbluetoothservices.sh
@@ -15,8 +15,6 @@ set -oue pipefail
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-systemctl disable --user obex.service
-systemctl mask --user obex.service
 
 systemctl disable bluetooth.service
 systemctl mask bluetooth.service

a3c9b90346dff2c7643b2e7ca5bb9c6c50f2fccf
Exponent64 <259167702+Exponent64@users.noreply.github.com>
2026-06-12 16:59:24 +0100

    Codacy is a single quotes hater

--- files/system/usr/libexec/secureblue/bios.py
+++ files/system/usr/libexec/secureblue/bios.py
@@ -9,8 +9,8 @@
 from os import path
 from subprocess import run
 
-if path.exists('/sys/firmware/efi'):
-    proceed = input('The system will reboot into UEFI firmware settings. Proceed? (y/N) ')
+if path.exists("/sys/firmware/efi"):
+    proceed = input("The system will reboot into UEFI firmware settings. Proceed? (y/N) ")
     if proceed.lower() in ["yes", "y"]:
         run(["/usr/bin/systemctl", "reboot", "--firmware-setup"], check=True)
 else:

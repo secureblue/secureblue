fc58f649832924e4c5ad483ad909ec0b8c8c6d1d
Exponent64 <259167702+Exponent64@users.noreply.github.com>
2026-06-12 16:22:53 +0100

    Rewrite ujust bios in Python

---
+++ files/system/usr/libexec/secureblue/bios.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# SPDX-FileCopyrightText: Copyright 2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Boot into this device's BIOS/UEFI screen."""
+
+from os import path
+from subprocess import run
+
+if path.exists('/sys/firmware/efi'):
+    proceed = input('The system will reboot into UEFI firmware settings. Proceed? (y/N) ')
+    if proceed.lower() in ["yes", "y"]:
+        run(["/usr/bin/systemctl", "reboot", "--firmware-setup"], check=True)
+else:
+    print("Rebooting to legacy BIOS from OS is not supported.")

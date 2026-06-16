8148ab8301b5d071867dcfbc5f754966e51476db
francoism90 <francoism90@users.noreply.github.com>
2026-06-16 09:29:24 +0200

    Use list instead loop

--- files/system/usr/libexec/secureblue/inner/network_filesystems.py
+++ files/system/usr/libexec/secureblue/inner/network_filesystems.py
@@ -12,24 +12,42 @@ import os
 import sys
 from typing import Final
 
-from utils import SystemdService
+NETFS_MODULES_FILE: Final[str] = "/etc/modprobe.d/99-network-filesystems.conf"
 
-NETFS_MOD_FILE: Final[str] = "/etc/modprobe.d/99-network-filesystems.conf"
-NETFS_MOD_TEXT: Final[str] = """install nfs /sbin/modprobe --ignore-install nfs
-install nfsv4 /sbin/modprobe --ignore-install nfsv4
-install nfs_acl /sbin/modprobe --ignore-install nfs_acl
-install nfs_localio /sbin/modprobe --ignore-install nfs_localio
-install nfsd /sbin/modprobe --ignore-install nfsd
-install nfs_layout_flexfiles /sbin/modprobe --ignore-install nfs_layout_flexfiles
-install nfs_layout_nfsv41_files /sbin/modprobe --ignore-install nfs_layout_nfsv41_files
-install grace /sbin/modprobe --ignore-install grace
-install lockd /sbin/modprobe --ignore-install lockd
-install auth_rpcgss /sbin/modprobe --ignore-install auth_rpcgss
-install rpcsec_gss_krb5 /sbin/modprobe --ignore-install rpcsec_gss_krb5
-install sunrpc /sbin/modprobe --ignore-install sunrpc
-install cifs /sbin/modprobe --ignore-install cifs
-install ksmbd /sbin/modprobe --ignore-install ksmbd
-"""
+NETFS_MODULES: Final[list[str]] = [
+    "nfsv4",
+    "nfs_acl",
+    "nfs_localio",
+    "nfsd",
+    "nfs_layout_flexfiles",
+    "nfs_layout_nfsv41_files",
+    "grace",
+    "lockd",
+    "auth_rpcgss",
+    "rpcsec_gss_krb5",
+    "sunrpc",
+    "cifs",
+    "ksmbd",
+]
+
+NETFS_SERVICES: Final[list[str]] = [
+    "nfs-idmapd.service",
+    "nfs-client.target",
+    "nfs-blkmap.service",
+    "nfs-mountd.service",
+    "nfsdcld.service",
+    "nfs-server.service",
+    "nfs-utils.service",
+    "rpc-gssd.service",
+    "rpc-statd-notify.service",
+    "rpc-statd.service",
+    "rpcbind.service",
+    "rpcbind.socket",
+    "rpcbind.target",
+    "rpc_pipefs.target",
+    "var-lib-nfs-rpc_pipefs.mount",
+    "gssproxy.service"
+]
 
 
 def main() -> int:
@@ -37,42 +55,23 @@ def main() -> int:
     required_args_count = 2
     if len(sys.argv) != required_args_count:
         return 1
-    netfs_services = [
-        "nfs-idmapd.service",
-        "nfs-client.target",
-        "nfs-blkmap.service",
-        "nfs-mountd.service",
-        "nfsdcld.service",
-        "nfs-server.service",
-        "nfs-utils.service",
-        "rpc-gssd.service",
-        "rpc-statd-notify.service",
-        "rpc-statd.service",
-        "rpcbind.service",
-        "rpcbind.socket",
-        "rpcbind.target",
-        "rpc_pipefs.target",
-        "var-lib-nfs-rpc_pipefs.mount",
-        "gssproxy.service"
-    ]
     mode = sys.argv[1]
     match mode:
         case "on":
-            with open(NETFS_MOD_FILE, "w", encoding="utf8") as fd:
-                fd.write(NETFS_MOD_TEXT)
-            os.chmod(NETFS_MOD_FILE, 0o644)
+            with open(NETFS_MODULES_FILE, "w", encoding="utf8") as fd:
+                for module in NETFS_MODULES:
+                    fd.write(f"install {module} /sbin/modprobe --ignore-install {module}\n")
+            os.chmod(NETFS_MODULES_FILE, 0o644)
 
-            for netfs_service in netfs_services:
-                SystemdService(netfs_service).unmask()
+            subprocess.run(["/usr/bin/systemctl", "unmask", "--", *NETFS_SERVICES], check=True)
 
             print("Network filesystems has been enabled. Reboot for effect.")
             return 0
         case "off":
-            os.remove(NETFS_MOD_FILE)
+            os.remove(NETFS_MODULES_FILE)
 
-            for netfs_service in netfs_services:
-                SystemdService(netfs_service).disable_now()
-                SystemdService(netfs_service).mask()
+            subprocess.run(["/usr/bin/systemctl", "disable", "--now", "--", *NETFS_SERVICES], check=True)
+            subprocess.run(["/usr/bin/systemctl", "mask", "--", *NETFS_SERVICES], check=True)
 
             print("Network filesystems has been disabled. Reboot for effect.")
             return 0

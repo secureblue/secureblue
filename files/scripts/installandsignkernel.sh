5b8c403835c85265b60e0250885e8d1123a3ac0d
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-06-20 18:34:32 -0700

    feat: swap for our kernel

--- files/scripts/installandsignkernel.sh
+++ files/scripts/installandsignkernel.sh
@@ -9,6 +9,9 @@ set -euo pipefail
 
 KERNEL_VERSION="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
+
+dnf install --repo "copr:copr.fedorainfracloud.org:secureblue:packages" "kernel-${KERNEL_VERSION}.secureblue"
+
 PUBLIC_KEY_DER_PATH='../system/usr/share/pki/akmods/certs/akmods-secureblue.der'
 PUBLIC_KEY_CRT_PATH='./certs/public_key.crt'
 PRIVATE_KEY_PATH='/tmp/certs/private_key.priv'

33e269d696195ecb3a80096e65adbe541175d952
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-06-20 20:46:57 -0700

    fix: add -y

--- files/scripts/installandsignkernel.sh
+++ files/scripts/installandsignkernel.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 KERNEL_VERSION="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 
-dnf install --repo "copr:copr.fedorainfracloud.org:secureblue:packages" "kernel-${KERNEL_VERSION/.fc/.secureblue.fc}"
+dnf install --repo "copr:copr.fedorainfracloud.org:secureblue:packages" "kernel-${KERNEL_VERSION/.fc/.secureblue.fc}" -y
 
 PUBLIC_KEY_DER_PATH='../system/usr/share/pki/akmods/certs/akmods-secureblue.der'
 PUBLIC_KEY_CRT_PATH='./certs/public_key.crt'

13ce1d282989116b2d8927cfe26cca0682f6308e
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-06-22 20:20:21 -0700

    fix: temp allow kernel installation

--- files/scripts/installandsignkernel.sh
+++ files/scripts/installandsignkernel.sh
@@ -10,8 +10,12 @@ set -euo pipefail
 KERNEL_VERSION="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 SECUREBLUE_KERNEL_VERSION="${KERNEL_VERSION/.fc/.secureblue.*.fc}"
 
-
+SECUREBLUE_PACKAGES_REPO_PATH="/etc/yum.repos.d/secureblue-packages-fedora.repo"
+SECUREBLUE_PACKAGES_REPO_COPY_PATH="/etc/yum.repos.d/secureblue-packages-fedora.repo"
+cp "${SECUREBLUE_PACKAGES_REPO_PATH}" "${SECUREBLUE_PACKAGES_REPO_COPY_PATH}"
+sed -i '/^includepkgs=/d' "${SECUREBLUE_PACKAGES_REPO_PATH}"
 dnf install --repo "copr:copr.fedorainfracloud.org:secureblue:packages" "kernel-${SECUREBLUE_KERNEL_VERSION}" -y
+cp "${SECUREBLUE_PACKAGES_REPO_COPY_PATH}" "${SECUREBLUE_PACKAGES_REPO_PATH}"
 
 PUBLIC_KEY_DER_PATH='../system/usr/share/pki/akmods/certs/akmods-secureblue.der'
 PUBLIC_KEY_CRT_PATH='./certs/public_key.crt'

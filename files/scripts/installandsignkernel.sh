8fad2580281db32af62719cd91dc6cecaac850d7
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-06-23 15:26:09 -0700

    fix: new kernel version

--- files/scripts/installandsignkernel.sh
+++ files/scripts/installandsignkernel.sh
@@ -17,6 +17,8 @@ sed -i '/^includepkgs=/d' "${SECUREBLUE_PACKAGES_REPO_PATH}"
 dnf install --repo "copr:copr.fedorainfracloud.org:secureblue:packages" "kernel-${SECUREBLUE_KERNEL_VERSION}" -y
 cp "${SECUREBLUE_PACKAGES_REPO_COPY_PATH}" "${SECUREBLUE_PACKAGES_REPO_PATH}"
 
+
+SECUREBLUE_NEW_KERNEL_VERSION="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 PUBLIC_KEY_DER_PATH='../system/usr/share/pki/akmods/certs/akmods-secureblue.der'
 PUBLIC_KEY_CRT_PATH='./certs/public_key.crt'
 PRIVATE_KEY_PATH='/tmp/certs/private_key.priv'
@@ -24,6 +26,6 @@ PRIVATE_KEY_PATH='/tmp/certs/private_key.priv'
 openssl x509 -in "${PUBLIC_KEY_DER_PATH}" -out "${PUBLIC_KEY_CRT_PATH}"
 sbsign --cert "${PUBLIC_KEY_CRT_PATH}" \
     --key "${PRIVATE_KEY_PATH}" \
-    --output "/usr/lib/modules/${SECUREBLUE_KERNEL_VERSION}/vmlinuz" \
-    "/usr/lib/modules/${SECUREBLUE_KERNEL_VERSION}/vmlinuz"
-sbverify --list "/usr/lib/modules/${SECUREBLUE_KERNEL_VERSION}/vmlinuz"
+    --output "/usr/lib/modules/${SECUREBLUE_NEW_KERNEL_VERSION}/vmlinuz" \
+    "/usr/lib/modules/${SECUREBLUE_NEW_KERNEL_VERSION}/vmlinuz"
+sbverify --list "/usr/lib/modules/${SECUREBLUE_NEW_KERNEL_VERSION}/vmlinuz"

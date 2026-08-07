59106f86f3e983a74b776ad5ffc848f2585b49ba
alexvojproc <16513623+alexvojproc@users.noreply.github.com>
2026-08-07 16:21:32 +0100

    feat(uki): download directly to disk

--- uki/install.sh
+++ uki/install.sh
@@ -12,11 +12,18 @@ github_repo_owner="secureblue"
 github_repo_name="secureblue"
 ghcr_tag="br-testing-44-uki"
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "This script must be run as root."
+if [[ $(id -u) -ne 0 ]]; then
+    echo "The installer must be run as root."
     exit 1
 fi
 
+total_ram_kb=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+if [[ "${total_ram_kb}" -lt 3800000 ]]; then
+    echo "The installer requires at least 4 GB of RAM."
+    exit 1
+fi
+
+# Select an image.
 valid_images=(
     silverblue-main-hardened
     kinoite-main-hardened
@@ -25,6 +32,7 @@ valid_images=(
     iot-main-hardened
     securecore-main-hardened
 )
+clear
 echo "Choose an image to install:"
 printf -- "- %s\n" "${valid_images[@]}"
 read -r -p "Enter a full image name: " ghcr_image
@@ -89,8 +97,8 @@ full_ref=$(crane digest --full-ref "${image_ref}")
 slsa-verifier verify-image --source-uri "github.com/${github_repo_owner}/${github_repo_name}" "${full_ref}"
 
 # Partition the disk.
-esp_uuid=$(uuidgen)
-root_uuid=$(uuidgen)
+esp_uuid=$(systemd-id128 new --uuid)
+root_uuid=$(systemd-id128 new --uuid)
 mkdir -p /run/repart.d
 cat > /run/repart.d/01-esp.conf << EOF
 [Partition]
@@ -109,7 +117,9 @@ SizeMinBytes=20G
 Encrypt=key-file
 EOF
 
+clear
 lsblk
+echo
 echo "Choose a disk to install to, e.g. /dev/vda, /dev/sda or /dev/nvme0n1."
 echo "This will erase the disk."
 read -r -p "Enter disk: " disk
@@ -118,6 +128,7 @@ if [[ ! -b "${disk}" ]] || [[ $(lsblk -ndo TYPE "${disk}") != "disk" ]]; then
     exit 1
 fi
 
+echo
 echo "Choose a LUKS encryption password."
 read -r -s -p "Password: " password && echo
 read -r -s -p "Confirm: " password2 && echo
@@ -131,8 +142,13 @@ fi
 unset password password2
 
 cleanup() {
-    rm -f "${keyfile}"
-    umount /var/lib/containers/storage/ || true
+    # Clean up /tmp, swap, then /mnt/tmp, then unmount everything else.
+    rm -rf /tmp/*
+    mount -o remount,size=1G /tmp
+    swapoff /mnt/tmp/swap/swapfile || true
+    umount /var/lib/containers/storage || true
+    rm -rf /mnt/tmp
+    sync
     umount /mnt/boot/ || true
     umount /mnt/ || true
     cryptsetup close root || true
@@ -148,16 +164,35 @@ mount /dev/mapper/root /mnt/
 mkdir /mnt/boot/
 mount "/dev/disk/by-partuuid/${esp_uuid}" /mnt/boot/
 
-# Pull the image.
-mount -t tmpfs -o size=10240M containers /var/lib/containers/storage/
+# Use /mnt/tmp as a temporary download location, rather than using RAM.
+mkdir -p /mnt/tmp/storage
+mount --bind /mnt/tmp/storage /var/lib/containers/storage
 chcon "system_u:object_r:container_var_lib_t:s0" /var/lib/containers/storage
+mkdir /mnt/tmp/tmp
+export TMPDIR=/mnt/tmp/tmp
+mkdir /mnt/tmp/swap
+chattr +C /mnt/tmp/swap
+fallocate -l 4G /mnt/tmp/swap/swapfile
+chmod 0600 /mnt/tmp/swap/swapfile
+mkswap /mnt/tmp/swap/swapfile
+swapon /mnt/tmp/swap/swapfile
+mount -o remount,size=8G /tmp
+mkdir /mnt/tmp/empty
+
+# Pull the image.
+clear
+echo "Downloading secureblue..."
 podman pull "${full_ref}"
 
 # Perform the installation.
+clear
+echo "Installing secureblue, this may take some time..."
 podman run --rm --privileged --pid=host --ipc=host \
     --security-opt label=type:unconfined_t \
-    -v /var/lib/containers:/var/lib/containers -v /dev:/dev \
+    -v /var/lib/containers:/var/lib/containers \
+    -v /dev:/dev \
     -v /:/run/host \
+    -v /mnt/tmp/empty:/run/host/mnt/tmp \
     -v "/etc/pki/containers/${github_repo_owner}.pub:/etc/pki/containers/${github_repo_owner}.pub:ro" \
     -v /etc/containers/policy.json:/etc/containers/policy.json:ro \
     "${full_ref}" \
@@ -198,17 +233,40 @@ EOF
 mkdir -p "${etc_dir}/systemd/system/local-fs.target.wants"
 ln -s /etc/systemd/system/boot.mount "${etc_dir}/systemd/system/local-fs.target.wants/boot.mount"
 
+# Create user.
+clear
+echo "Enter the details of your login account. It will be added to the admin (wheel) group."
+echo "You should NOT choose a system username, like 'root' or 'admin'."
+echo "Your password will be set to 'secureblue'. You must change it after logging in."
+echo
+read -r -p "Username: " username
+password=$(openssl passwd -6 "secureblue")
+last_changed=$(( $(date +%s) / 86400 ))
+echo "${username}:x:1000:1000:${username}:/var/home/${username}:/bin/bash" >> "${etc_dir}/passwd"
+echo "${username}:${password}:${last_changed}:0:99999:7:::" >> "${etc_dir}/shadow"
+echo "${username}:x:1000:" >> "${etc_dir}/group"
+sed -i "s/^wheel:x:10:$/&${username}/" "${etc_dir}/group"
+var_dir="/mnt/state/os/default/var"
+mkdir -p "${var_dir}/home/${username}/"
+cp -a "${etc_dir}/skel/." "${var_dir}/home/${username}/"
+chown -R 1000:1000 "${var_dir}/home/${username}"
+chmod 0700 "${var_dir}/home/${username}"
+
 # Optionally install shim.
-cat << EOF
+clear
+cat << 'EOF'
 Choose a secure boot option:
+
 1. shim - Safe for all devices.
    Your firmware must trust the Microsoft Third Party CA, which signs many
    bootloaders and ROMs. This greatly increases attack surface but is compatible
    with all hardware.
-2. secureblue Platform Key - Suitable for some devices.
+
+2. secureblue Platform Key - WARNING: Not suitable for all devices.
    Your firmware will only trust secureblue. This offers improved security, but
    this can BRICK YOUR DEVICE by breaking the display if your external GPU
    requires an option ROM to work.
+
 EOF
 read -r -p "Choose an option (1 or 2): " secure_boot
 if [[ "${secure_boot}" != "2" ]]; then
@@ -231,18 +289,34 @@ if [[ "${secure_boot}" != "2" ]]; then
     mv /mnt/boot/EFI/systemd/systemd-bootx64.efi /mnt/boot/EFI/fedora/grubx64.efi
     rmdir /mnt/boot/EFI/systemd
     # We need to import the key that signed systemd-boot.
-    printf "secureblue\nsecureblue\n" | mokutil --import keys/db/db.der
+    printf "secureblue\nsecureblue\n" | mokutil --import keys/db/db.der > /dev/null
 fi
 
-sync
+cleanup
 
 # Reboot the system.
+clear
+cat << EOF
+Installation is finished.
+
+Username: ${username}
+Password: secureblue
+
+EOF
 if [[ "${secure_boot}" == "1" ]]; then
-    echo "Installation is finished. When the system reboots, you need to choose"
-    echo "\"Enroll MOK\" and enter \"secureblue\" as the password."
+cat << EOF
+When the system reboots, you need to choose "Enroll MOK" and enter "secureblue"
+as the password.
+EOF
 else
-    echo "Installation is finished. Make sure your device is in Setup Mode by deleting"
-    echo "the Secure Boot Platform Key in your UEFI settings."
+cat << 'EOF'
+After logging in, you must run `ujust enroll-secure-boot-key` to finish setting
+up secure boot.
+
+Make sure your device is in Setup Mode first by deleting the Platform Key in
+your UEFI settings.
+
+EOF
 fi
-read -r -p "Press enter to reboot."
+read -r -p "Press enter to reboot or Ctrl+C to exit to terminal."
 systemctl reboot

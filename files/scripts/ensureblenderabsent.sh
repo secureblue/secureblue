1eec2857b2c416d23fc124985c92e53cc484a7d8
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-09-10 13:54:21 -0700

    feat: proactive safeguards against packaging bugs (#2627)

---
+++ files/scripts/ensureblenderabsent.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+if rpm -q blender &> /dev/null
+then
+    echo "blender found. Exiting..."
+    exit 1
+fi

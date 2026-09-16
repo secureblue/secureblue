0afb30d0383924ebb07d12e0f529e73db710f900
Commenter <c@commenter.cc>
2026-09-16 05:53:52 -0500

    fix(bash-prompt): prevent undesired xtrace output

--- files/system/etc/profile.d/xx-bash-prompt-command.sh
+++ files/system/etc/profile.d/xx-bash-prompt-command.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/bash
 # Filename needs to be alphabetically later than vte.sh
+
+# Print nonzero exit codes in bold red text inside square brackets at beginning of prompt
 if [[ $- == *i* && -n "${BASH_VERSION:-}" ]]; then
-    # Print nonzero exit codes in bold red text inside square brackets at beginning of prompt
+    __scbl_ps1_exit_code() {
+        # Only print the function name during xtrace for cleaner output
+        { local -; set +x; } 2>/dev/null
+
+        local start
+        start+=$'\e[31m' # red
+        start+="["
+        start+=$'\e[1m' # bold
+
+        local end
+        end+=$'\e[22m' # default intensity
+        end+="]"
+        end+=$'\e[39m' # default color
+
+        local -r code=${1#0}
+        echo -n "${code:+${start}${code}${end}}"
+    }
+
     # shellcheck disable=SC2154
-    PS1='$(code=${?#0};echo -n "${code:+\[\e[31m\][\[\e[1m\]${code}\[\e[22m\]]\[\e[39m\]}")'"${PS1:-}"
+    PS1='$(__scbl_ps1_exit_code $?)'"${PS1:-}"
 fi
